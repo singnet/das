fb1f0279a0ee8853a84decae0f55bf0184a6bb0c
Levi <141682181+levisingularity@users.noreply.github.com>
2024-03-28 08:23:24 -0300

    [#11] Add metta parser and toolbox as packages to be updated in the release script (#12)
    * Ref
    
    * Fix version init
    
    * Organize reusable hooks into folders
    
    * Add das mettaparser and das toolbox

--- deployment/hooks/poetry-before-release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-local poetry_version="$new_package_version"
-
-poetry version $poetry_version
---
+++ deployment/hooks/poetry/before-release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+local poetry_version="$new_package_version"
+local package_folder_path="${package_name//-/_}"
+local init_file_path=$package_folder_path/__init__.py
+
+poetry version $poetry_version
+
+if [ -f "$init_file_path" ]; then
+    sed -i "s/__version__ = .*/__version__ = '$poetry_version'/g" "$package_folder_path/__init__.py"
+fi
