195ed1aa517d0a697e763bc7d1863b0c4275a4df
levisingularity <rafael.levi@singularitynet.io>
2024-04-29 23:52:43 -0300

    Remove server

--- scripts/deployment/fn_deploy.sh
+++ scripts/deployment/fn_deploy.sh
@@ -4,6 +4,8 @@ set -e
 
 # PATHS
 workdir=$(pwd)
+das_dir="$HOME/.das"
+
 source "$workdir/scripts/deployment/utils.sh"
 
 # START DEPLOYMENT
@@ -21,8 +23,85 @@ source "$workdir/scripts/deployment/utils.sh"
 # ROOT
 # PEM OR PASSWORD
 
+function display_server_aliases() {
+    local json_filename="$1"
+    local aliases=$(jq -r '.servers[].alias' "$json_filename")
+    echo "Servidores registrados:"
+    echo "$aliases"
+}
+
+function remove_server() {
+    echo -e "\nRemove Server\n"
+
+    local das_dir="$HOME/.das"
+    local json_filename="$das_dir/data.json"
+
+    if [[ -f "$json_filename" ]]; then
+        display_server_aliases "$json_filename"
+
+        local alias_to_remove=$(text_prompt "Alias do servidor a ser removido: ")
+        local existing_data=$(cat "$json_filename")
+
+        if [[ $(echo "$existing_data" | jq '.servers') != "null" ]]; then
+            existing_data=$(echo "$existing_data" | jq 'del(.servers[] | select(.alias == "'"$alias_to_remove"'"))')
+
+            echo "$existing_data" >"$json_filename"
+            echo "Servidor removido com sucesso."
+        else
+            echo "Não há servidores registrados para remover."
+        fi
+    else
+        echo "O arquivo JSON não existe. Nenhum servidor foi removido."
+    fi
+}
+
 function register_server() {
-    echo ""
+    print "\nRegister Server\n"
+
+    local alias=$(text_prompt "Alias: ")
+    local ip=$(text_prompt "IP: ")
+    local username=$(text_prompt "Username: ")
+    local pem_or_password=$(text_prompt "Pem key or Password: ")
+    local is_pem=""
+    local json_filename="$das_dir/data.json"
+
+    if is_pem_file "$pem_or_password" &>/dev/null; then
+        is_pem=true
+    else
+        is_pem=false
+        pem_or_password=$(encrypt_password "$pem_or_password" "mykey")
+    fi
+
+    append_server_data_to_json "$alias" "$ip" "$username" "$is_pem" "$pem_or_password" "$json_filename"
+}
+
+function append_server_data_to_json() {
+    local alias="$1"
+    local ip="$2"
+    local username="$3"
+    local is_pem="$4"
+    local pem_or_password="$5"
+    local json_filename="$6"
+
+    local data='{"alias": "'"$alias"'", "ip": "'"$ip"'", "username": "'"$username"'", "is_pem": '"$is_pem"', "password": "'"$pem_or_password"'"}'
+
+    if [[ -f "$json_filename" ]]; then
+        local existing_data=$(cat "$json_filename")
+
+        if [[ $(echo "$existing_data" | jq '.servers') != "null" ]]; then
+            existing_data=$(echo "$existing_data" | jq '.servers += ['"$data"']')
+        else
+            existing_data=$(echo "$existing_data" | jq '. + {"servers": ['"$data"']}')
+        fi
+
+        echo "$existing_data" >"$json_filename"
+    else
+        local new_data='{"servers": ['"$data"']}'
+        mkdir -p "$das_dir"
+        echo "$new_data" >"$json_filename"
+    fi
+
+    echo "Dados salvos com sucesso em: $json_filename"
 }
 
 function start_deployment() {
@@ -32,6 +111,7 @@ function start_deployment() {
 function main() {
     local options=(
         "Register Server"
+        "Remove Server"
         "Start Deployment"
         "Exit"
     )
@@ -39,11 +119,14 @@ function main() {
     choose_menu "Please choose an option:" selected_option "${options[@]}"
 
     case $selected_option in
-    "Start Deployment")
-        start_deployment
-        ;;
     "Register Server")
         register_server
+        ;;
+    "Remove Server")
+        remove_server
+        ;;
+    "Start Deployment")
+        start_deployment
         ;;
     "Exit")
         echo "Saindo..."
