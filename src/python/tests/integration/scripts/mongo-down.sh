e1720026c23218c5107ff56c53033ffbbe67ba36
Angelo Probst <angelo.probst@gmail.com>
2025-01-10 17:38:47 -0300

    making AtomDB unit tests runnable

---
+++ src/python/tests/integration/scripts/mongo-down.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "Usage: mongo-down.sh PORT"
+    exit 1
+else
+    PORT=$1
+fi
+
+echo "Destroying MongoDB container on port $PORT"
+
+docker stop mongo_$PORT && \
+  docker kill mongo_$PORT &&
+  docker rm mongo_$PORT && \
+  docker volume rm mongodbdata_$PORT >& /dev/null
