c0845bbd0f73b37361c83afd68ecf71a8b3f2744
Pedro Costa <pedro.costa@ibm.com>
2025-02-07 18:00:59 +0000

    reviewers comments

--- src/scripts/bazel_build.sh
+++ src/scripts/bazel_build.sh
@@ -29,44 +29,35 @@ if [ "$BUILD_BINARIES" = false ] && [ "$BUILD_WHEELS" = false ]; then
 fi
 
 if [ "$BUILD_BINARIES" = true ]; then
-    cd "$CPP_WORKSPACE_DIR" \
-    && $BAZELISK_BUILD_CMD //:link_creation_server \
-    && mv bazel-bin/link_creation_server "$BIN_DIR" \
-    && $BAZELISK_BUILD_CMD //:link_creation_agent_client \
-    && mv bazel-bin/link_creation_agent_client "$BIN_DIR" \
-    && $BAZELISK_BUILD_CMD //:word_query \
-    && mv bazel-bin/word_query "$BIN_DIR" \
-    && $BAZELISK_BUILD_CMD //:attention_broker_service \
-    && mv bazel-bin/attention_broker_service "$BIN_DIR" \
-    && $BAZELISK_BUILD_CMD //:query_broker \
-    && mv bazel-bin/query_broker "$BIN_DIR" \
-    && $BAZELISK_BUILD_CMD //:query \
-    && mv bazel-bin/query "$BIN_DIR"
+    cd "$CPP_WORKSPACE_DIR"
+    $BAZELISK_BUILD_CMD //:link_creation_server
+    mv bazel-bin/link_creation_server "$BIN_DIR"
+    $BAZELISK_BUILD_CMD //:link_creation_agent_client
+    mv bazel-bin/link_creation_agent_client "$BIN_DIR"
+    $BAZELISK_BUILD_CMD //:word_query
+    mv bazel-bin/word_query "$BIN_DIR"
+    $BAZELISK_BUILD_CMD //:attention_broker_service
+    mv bazel-bin/attention_broker_service "$BIN_DIR"
+    $BAZELISK_BUILD_CMD //:query_broker
+    mv bazel-bin/query_broker "$BIN_DIR"
+    $BAZELISK_BUILD_CMD //:query
+    mv bazel-bin/query "$BIN_DIR"
 fi
 
-if [ "${?}" = "0" -a "$BUILD_WHEELS" = true ]; then
-    cd "$PYTHON_WORKSPACE_DIR" \
-    && $BAZELISK_RUN_CMD \
-        //deps:requirements.update \
-    && $BAZELISK_RUN_CMD \
-        //deps:requirements_dev.update \
-    && $BAZELISK_BUILD_CMD \
-        //hyperon_das_atomdb:hyperon_das_atomdb_wheel \
-        --define=ATOMDB_VERSION=0.8.11 \
-    && $BAZELISK_BUILD_CMD \
-        //hyperon_das:hyperon_das_wheel \
-        --define=DAS_VERSION=0.9.17 \
-    && mv bazel-bin/hyperon_das_atomdb/*.whl "$BIN_DIR" \
-    && mv bazel-bin/hyperon_das/*.whl "$BIN_DIR" \
-    && cd "$CPP_WORKSPACE_DIR" \
-    && $BAZELISK_BUILD_CMD \
-        //hyperon_das_node:hyperon_das_node_wheel \
-        --define=DAS_NODE_VERSION=0.0.1 \
-    && mv bazel-bin/hyperon_das_node/*.whl "$BIN_DIR" \
-    && $BAZELISK_BUILD_CMD \
-        //hyperon_das_atomdb_cpp:hyperon_das_atomdb_cpp_wheel \
-        --define=ATOMDB_VERSION=0.8.11 \
-    && mv bazel-bin/hyperon_das_atomdb_cpp/*.whl "$BIN_DIR"
+if [ "$BUILD_WHEELS" = true ]; then
+    cd "$PYTHON_WORKSPACE_DIR"
+    $BAZELISK_RUN_CMD //deps:requirements.update
+    $BAZELISK_RUN_CMD //deps:requirements_dev.update
+    $BAZELISK_BUILD_CMD //hyperon_das_atomdb:hyperon_das_atomdb_wheel --define=ATOMDB_VERSION=0.8.11
+    $BAZELISK_BUILD_CMD //hyperon_das:hyperon_das_wheel --define=DAS_VERSION=0.9.17
+    mv bazel-bin/hyperon_das_atomdb/*.whl "$BIN_DIR"
+    mv bazel-bin/hyperon_das/*.whl "$BIN_DIR"
+    cd "$CPP_WORKSPACE_DIR"
+    $BAZELISK_BUILD_CMD //hyperon_das_node:hyperon_das_node_wheel --define=DAS_NODE_VERSION=0.0.1
+    mv bazel-bin/hyperon_das_node/*.whl "$BIN_DIR"
+    $BAZELISK_BUILD_CMD //hyperon_das_atomdb_cpp:hyperon_das_atomdb_cpp_wheel \
+      --define=ATOMDB_VERSION=0.8.11
+    mv bazel-bin/hyperon_das_atomdb_cpp/*.whl "$BIN_DIR"
 fi
 
 exit $?
