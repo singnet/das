0385b7b2b10eabb422fc5041675d2fa1ff00b4ad
levisingularity <rafael.levi@singularitynet.io>
2025-02-10 15:07:38 -0300

    das-250: set runs-on as self-hosted

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -8,10 +8,10 @@ CONTAINER_NAME=${IMAGE_NAME}-container
 # local paths
 LOCAL_WORKDIR=$(pwd)
 LOCAL_BIN_DIR=$LOCAL_WORKDIR/bin
-LOCAL_BAZEL_CACHE=$LOCAL_WORKDIR/docker/volumes/cache/bazel
-LOCAL_BAZELISK_CACHE=$LOCAL_WORKDIR/docker/volumes/cache/bazelisk
-LOCAL_PIPTOOLS_CACHE=$LOCAL_WORKDIR/docker/volumes/cache/pip-tools
-LOCAL_PIP_CACHE=$LOCAL_WORKDIR/docker/volumes/cache/pip
+LOCAL_BAZEL_CACHE="$HOME/.cache/das/bazel"
+LOCAL_BAZELISK_CACHE="$HOME/.cache/das/bazelisk"
+LOCAL_PIPTOOLS_CACHE="$HOME/.cache/das/pip-tools"
+LOCAL_PIP_CACHE="$HOME/.cache/das/pip"
 mkdir -p \
     $LOCAL_BAZEL_CACHE \
     $LOCAL_BAZELISK_CACHE \
@@ -31,18 +31,18 @@ CONTAINER_WORKSPACE_DIR_PYTHON=$CONTAINER_WORKDIR/python
 
 
 docker run --rm \
-    --user=$(id -u):$(id -g) \
+    --user="$(id -u):$(id -g)" \
     --name=$CONTAINER_NAME \
     -e BIN_DIR=$CONTAINER_BIN_DIR \
     -e CPP_WORKSPACE_DIR=$CONTAINER_WORKSPACE_DIR_CPP \
     -e PYTHON_WORKSPACE_DIR=$CONTAINER_WORKSPACE_DIR_PYTHON \
     --volume /etc/passwd:/etc/passwd:ro \
-    --volume $LOCAL_PIP_CACHE:$CONTAINER_PIP_CACHE \
-    --volume $LOCAL_PIPTOOLS_CACHE:$CONTAINER_PIPTOOLS_CACHE \
-    --volume $LOCAL_BAZEL_CACHE:$CONTAINER_BAZEL_CACHE \
-    --volume $LOCAL_BAZELISK_CACHE:$CONTAINER_BAZELISK_CACHE \
-    --volume $LOCAL_WORKDIR:$CONTAINER_WORKDIR \
-    --workdir $CONTAINER_WORKDIR \
+    --volume "$LOCAL_PIP_CACHE:$CONTAINER_PIP_CACHE" \
+    --volume "$LOCAL_PIPTOOLS_CACHE:$CONTAINER_PIPTOOLS_CACHE" \
+    --volume "$LOCAL_BAZEL_CACHE:$CONTAINER_BAZEL_CACHE" \
+    --volume "$LOCAL_BAZELISK_CACHE:$CONTAINER_BAZELISK_CACHE" \
+    --volume "$LOCAL_WORKDIR:$CONTAINER_WORKDIR" \
+    --workdir "$CONTAINER_WORKDIR" \
     ${IMAGE_NAME} \
     ./scripts/bazel_build.sh
 
