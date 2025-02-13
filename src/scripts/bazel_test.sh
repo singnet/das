c06ec640627f75e9e2589c58c73731603280f8d6
Pedro Costa <pedro.costa@ibm.com>
2025-02-13 16:28:01 +0000

    test(skip): python failed due to infrastructure issues

--- src/scripts/bazel_test.sh
+++ src/scripts/bazel_test.sh
@@ -2,7 +2,7 @@
 
 (( JOBS=$(nproc)/2 ))
 BAZELISK_CMD=/opt/bazel/bazelisk
-BAZELISK_TEST_CMD="${BAZELISK_CMD} test --jobs ${JOBS} --enable_bzlmod --test_output=errors --build_tests_only"
+BAZELISK_TEST_CMD="${BAZELISK_CMD} test --jobs ${JOBS} --enable_bzlmod --test_output=errors --build_tests_only --test_tag_filters=-skip"
 
 $BAZELISK_TEST_CMD //...
 
