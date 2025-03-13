175aaae04b86af24b9973494a96e77a305c378b6
Pedro Costa <pedrobc89@gmail.com>
2025-03-13 01:25:27 +0000

    restore test_das_node

--- src/tests/python/unit/test_das_node.py
+++ src/tests/python/unit/test_das_node.py
@@ -62,38 +62,38 @@ class TestDASNode(TestCase):
         self.assertTrue(hasattr(RemoteIterator, "finished"))
         self.assertTrue(hasattr(RemoteIterator, "pop"))
 
-    # def test_das_node(self) -> None:
-    #     """
-    #     Test das_node server and client constructors
-    #     """
-    #     self.server_id: str = "localhost:35700"
-    #     self.client1_id: str = "localhost:35701"
-    #     self.client2_id: str = "localhost:35702"
-    #
-    #     self.server = DASNode(node_id=self.server_id)
-    #     self.client1 = DASNode(node_id=self.client1_id, server_id=self.server_id)
-    #     self.client2 = DASNode(node_id=self.client2_id, server_id=self.server_id)
-    #
-    #     # Test id assignment
-    #     self.assertEqual(self.server.node_id(), self.server_id)
-    #     self.assertEqual(self.client1.node_id(), self.client1_id)
-    #     self.assertEqual(self.client2.node_id(), self.client2_id)
-    #
-    #     # Server should be leader
-    #     self.assertTrue(self.server.has_leader())
-    #     self.assertTrue(self.client1.has_leader())
-    #     self.assertTrue(self.client2.has_leader())
-    #
-    #     self.assertTrue(self.server.is_leader())
-    #     self.assertFalse(self.client1.is_leader())
-    #     self.assertFalse(self.client2.is_leader())
-    #
-    #     self.assertEqual(self.server.leader_id(), self.server_id)
-    #     self.assertEqual(self.client1.leader_id(), self.server_id)
-    #     self.assertEqual(self.client2.leader_id(), self.server_id)
-    #
-    #     # Test id assignment
-    #     self.assertEqual(self.server.node_id(), self.server_id)
-    #     self.assertEqual(self.client1.node_id(), self.client1_id)
-    #     self.assertEqual(self.client2.node_id(), self.client2_id)
-    #
+    def test_das_node(self) -> None:
+        """
+        Test das_node server and client constructors
+        """
+        self.server_id: str = "localhost:35700"
+        self.client1_id: str = "localhost:35701"
+        self.client2_id: str = "localhost:35702"
+
+        self.server = DASNode(node_id=self.server_id)
+        self.client1 = DASNode(node_id=self.client1_id, server_id=self.server_id)
+        self.client2 = DASNode(node_id=self.client2_id, server_id=self.server_id)
+
+        # Test id assignment
+        self.assertEqual(self.server.node_id(), self.server_id)
+        self.assertEqual(self.client1.node_id(), self.client1_id)
+        self.assertEqual(self.client2.node_id(), self.client2_id)
+
+        # Server should be leader
+        self.assertTrue(self.server.has_leader())
+        self.assertTrue(self.client1.has_leader())
+        self.assertTrue(self.client2.has_leader())
+
+        self.assertTrue(self.server.is_leader())
+        self.assertFalse(self.client1.is_leader())
+        self.assertFalse(self.client2.is_leader())
+
+        self.assertEqual(self.server.leader_id(), self.server_id)
+        self.assertEqual(self.client1.leader_id(), self.server_id)
+        self.assertEqual(self.client2.leader_id(), self.server_id)
+
+        # Test id assignment
+        self.assertEqual(self.server.node_id(), self.server_id)
+        self.assertEqual(self.client1.node_id(), self.client1_id)
+        self.assertEqual(self.client2.node_id(), self.client2_id)
+
