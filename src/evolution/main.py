fdb24059a67fc3f5d101fefd61753c0747cc3ebc
marcocapozzoli <marcocapozzoli90@gmail.com>
2025-03-19 15:08:04 -0300

    Refactor main

--- src/evolution/main.py
+++ src/evolution/main.py
@@ -1,3 +1,5 @@
+import signal
+import sys
 import argparse
 
 from evolution.optimizer import QueryOptimizerAgent
@@ -15,29 +17,27 @@ def parse_args():
         default='config.cfg',
         help="Path to the configuration file (default: 'config.cfg')"
     )
-    # parser.add_argument(
-    #     '--query-tokens',
-    #     required=False,
-    #     help="Query tokens to be optimized"
-    # )
-
     return parser.parse_args()
+
+
+def shutdown_handler(signum, frame):
+    print("Stopping server...")
+    sys.exit(0)
 
 
 def main():
     try:
         args = parse_args()
         agent = QueryOptimizerAgent(config_file=args.config_file)
+
+        signal.signal(signal.SIGINT, shutdown_handler)
+        signal.signal(signal.SIGTERM, shutdown_handler)
+
         agent.run_server()
     except Exception as e:
-        raise e
+        print(f"An error occurred - Details: {str(e)}")
+        return 1
 
 
 if __name__ == '__main__':
-    def _sys():
-        import sys
-        sys.argv.extend([
-            '--config-file',
-            '/tmp/config.cfg'])
-    _sys()
-    print(main())
+    sys.exit(main())
