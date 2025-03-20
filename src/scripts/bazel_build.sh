e87cf4ff4ee64de43b8fba3390cbdaa4f14ea9f9
Pedro Costa <pedrobc89@gmail.com>
2025-03-20 15:42:42 +0000

    feature(build): faster build-all command

--- src/scripts/bazel_build.sh
+++ src/scripts/bazel_build.sh
@@ -26,21 +26,23 @@ if [ "$BUILD_BINARIES" = false ] && [ "$BUILD_WHEELS" = false ]; then
 fi
 
 if [ "$BUILD_BINARIES" = true ]; then
-    $BAZELISK_BUILD_CMD //:inference_agent_server
+    $BAZELISK_BUILD_CMD \
+      //:inference_agent_server \
+      //:inference_agent_client \
+      //:link_creation_server \
+      //:link_creation_agent_client \
+      //:word_query \
+      //:attention_broker_service \
+      //:query_broker \
+      //:query
+
     mv bazel-bin/inference_agent_server "$BIN_DIR"
-    $BAZELISK_BUILD_CMD //:inference_agent_client
     mv bazel-bin/inference_agent_client "$BIN_DIR"
-    $BAZELISK_BUILD_CMD //:link_creation_server
     mv bazel-bin/link_creation_server "$BIN_DIR"
-    $BAZELISK_BUILD_CMD //:link_creation_agent_client
     mv bazel-bin/link_creation_agent_client "$BIN_DIR"
-    $BAZELISK_BUILD_CMD //:word_query
     mv bazel-bin/word_query "$BIN_DIR"
-    $BAZELISK_BUILD_CMD //:attention_broker_service
     mv bazel-bin/attention_broker_service "$BIN_DIR"
-    $BAZELISK_BUILD_CMD //:query_broker
     mv bazel-bin/query_broker "$BIN_DIR"
-    $BAZELISK_BUILD_CMD //:query
     mv bazel-bin/query "$BIN_DIR"
 
 fi
@@ -48,13 +50,16 @@ fi
 if [ "$BUILD_WHEELS" = true ]; then
     $BAZELISK_RUN_CMD //deps:requirements.update
     $BAZELISK_RUN_CMD //deps:requirements_dev.update
-    $BAZELISK_BUILD_CMD //hyperon_das_atomdb:hyperon_das_atomdb_wheel
-    $BAZELISK_BUILD_CMD //hyperon_das:hyperon_das_wheel
+
+    $BAZELISK_BUILD_CMD \
+      //hyperon_das_atomdb:hyperon_das_atomdb_wheel \
+      //hyperon_das:hyperon_das_wheel \
+      //hyperon_das_node:hyperon_das_node_wheel \
+      //hyperon_das_atomdb_cpp:hyperon_das_atomdb_cpp_wheel
+
     mv bazel-bin/hyperon_das_atomdb/*.whl "$BIN_DIR"
     mv bazel-bin/hyperon_das/*.whl "$BIN_DIR"
-    $BAZELISK_BUILD_CMD //hyperon_das_node:hyperon_das_node_wheel
     mv bazel-bin/hyperon_das_node/*.whl "$BIN_DIR"
-    $BAZELISK_BUILD_CMD //hyperon_das_atomdb_cpp:hyperon_das_atomdb_cpp_wheel
     mv bazel-bin/hyperon_das_atomdb_cpp/*.whl "$BIN_DIR"
 fi
 
