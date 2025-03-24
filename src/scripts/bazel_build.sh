b14e1c3b7d14d9f0ccf584cf94ae171953d413d9
Pedro Costa <pedrobc89@gmail.com>
2025-03-24 15:18:53 +0000

    Only one build command

--- src/scripts/bazel_build.sh
+++ src/scripts/bazel_build.sh
@@ -6,6 +6,8 @@ BAZELISK_CMD=/opt/bazel/bazelisk
 BAZELISK_BUILD_CMD="${BAZELISK_CMD} build"
 [ "${BAZEL_JOBS:-x}" != "x" ] && BAZELISK_BUILD_CMD="${BAZELISK_BUILD_CMD} --jobs=${BAZEL_JOBS}"
 BAZELISK_RUN_CMD="${BAZELISK_CMD} run"
+BUILD_TARGETS=""
+MOVE_TARGETS=""
 
 for arg in "$@"; do
     case $arg in
@@ -27,25 +29,24 @@ if [ "$BUILD_BINARIES" = false ] && [ "$BUILD_WHEELS" = false ]; then
 fi
 
 if [ "$BUILD_BINARIES" = true ]; then
-    $BAZELISK_BUILD_CMD \
-      //:inference_agent_server \
-      //:inference_agent_client \
-      //:link_creation_server \
-      //:link_creation_agent_client \
-      //:word_query \
-      //:attention_broker_service \
-      //:query_broker \
-      //:query \
-      //evolution:main
+    BUILD_TARGETS+=" //:inference_agent_server"
+    BUILD_TARGETS+=" //:inference_agent_client"
+    BUILD_TARGETS+=" //:link_creation_server"
+    BUILD_TARGETS+=" //:link_creation_agent_client"
+    BUILD_TARGETS+=" //:word_query"
+    BUILD_TARGETS+=" //:attention_broker_service"
+    BUILD_TARGETS+=" //:query_broker"
+    BUILD_TARGETS+=" //:query"
+    BUILD_TARGETS+=" //evolution:main"
 
-    mv bazel-bin/inference_agent_server "$BIN_DIR"
-    mv bazel-bin/inference_agent_client "$BIN_DIR"
-    mv bazel-bin/link_creation_server "$BIN_DIR"
-    mv bazel-bin/link_creation_agent_client "$BIN_DIR"
-    mv bazel-bin/word_query "$BIN_DIR"
-    mv bazel-bin/attention_broker_service "$BIN_DIR"
-    mv bazel-bin/query_broker "$BIN_DIR"
-    mv bazel-bin/query "$BIN_DIR"
+    MOVE_TARGETS+=" bazel-bin/inference_agent_server"
+    MOVE_TARGETS+=" bazel-bin/inference_agent_client"
+    MOVE_TARGETS+=" bazel-bin/link_creation_server"
+    MOVE_TARGETS+=" bazel-bin/link_creation_agent_client"
+    MOVE_TARGETS+=" bazel-bin/word_query"
+    MOVE_TARGETS+=" bazel-bin/attention_broker_service"
+    MOVE_TARGETS+=" bazel-bin/query_broker"
+    MOVE_TARGETS+=" bazel-bin/query"
 
 fi
 
@@ -53,18 +54,20 @@ if [ "$BUILD_WHEELS" = true ]; then
     $BAZELISK_RUN_CMD //deps:requirements.update
     $BAZELISK_RUN_CMD //deps:requirements_dev.update
 
-    $BAZELISK_BUILD_CMD \
-      //hyperon_das_atomdb:hyperon_das_atomdb_wheel \
-      //hyperon_das:hyperon_das_wheel \
-      //hyperon_das_node:hyperon_das_node_wheel \
-      //hyperon_das_atomdb_cpp:hyperon_das_atomdb_cpp_wheel \
-      //hyperon_das_query_engine:hyperon_das_query_engine_wheel
+    BUILD_TARGETS+=" //hyperon_das_atomdb:hyperon_das_atomdb_wheel"
+    BUILD_TARGETS+=" //hyperon_das:hyperon_das_wheel"
+    BUILD_TARGETS+=" //hyperon_das_node:hyperon_das_node_wheel"
+    BUILD_TARGETS+=" //hyperon_das_atomdb_cpp:hyperon_das_atomdb_cpp_wheel"
+    BUILD_TARGETS+=" //hyperon_das_query_engine:hyperon_das_query_engine_wheel"
 
-    mv bazel-bin/hyperon_das_atomdb/*.whl "$BIN_DIR"
-    mv bazel-bin/hyperon_das/*.whl "$BIN_DIR"
-    mv bazel-bin/hyperon_das_node/*.whl "$BIN_DIR"
-    mv bazel-bin/hyperon_das_atomdb_cpp/*.whl "$BIN_DIR"
-    mv bazel-bin/hyperon_das_query_engine/*.whl "$BIN_DIR"
+    MOVE_TARGETS+=" bazel-bin/hyperon_das_atomdb/*.whl"
+    MOVE_TARGETS+=" bazel-bin/hyperon_das/*.whl"
+    MOVE_TARGETS+=" bazel-bin/hyperon_das_node/*.whl"
+    MOVE_TARGETS+=" bazel-bin/hyperon_das_atomdb_cpp/*.whl"
+    MOVE_TARGETS+=" bazel-bin/hyperon_das_query_engine/*.whl"
 fi
+
+$BAZELISK_BUILD_CMD $BUILD_TARGETS
+mv $MOVE_TARGETS $BIN_DIR
 
 exit $?
