07d152039a7b73a68dbc5750a05c6e82ef052fbc
Pedro Costa <pedrobc89@gmail.com>
2025-03-25 21:14:51 +0000

    move das_node binding to hyperon_das_node

--- src/hyperon_das_node/hyperon_das_node_ext.cc
+++ src/hyperon_das_node/hyperon_das_node_ext.cc
@@ -10,11 +10,19 @@
 #include "distributed_algorithm_node/MessageBroker.h"
 #include "distributed_algorithm_node/StarNode.h"
 
+#include <type_traits>
+
+#include "query_engine/DASNode.h"
+#include "query_engine/HandlesAnswer.h"
+#include "query_engine/QueryAnswer.h"
+#include "query_engine/query_element/RemoteIterator.h"
+
 namespace nb = nanobind;
 using namespace nb::literals;  // Enables use of literal "_a" for named arguments
 
 using namespace std;
 using namespace distributed_algorithm_node;
+using namespace query_engine;
 
 // Shared pointers aliases
 using MessagePtr = shared_ptr<Message>;
@@ -95,7 +103,7 @@ NB_MODULE(hyperon_das_node_ext, m) {
     // Needs to be updated whenever a new MessageBrokerType is added.
     nb::enum_<MessageBrokerType>(m, "MessageBrokerType")
         .value("GRPC", MessageBrokerType::GRPC)
-        .value("RAM", MessageBrokerType::RAM);
+        .value("RAM", MessageBrokerType::RAM)
         .export_values();
 
   // DistributedAlgorithmNode.h bindings
@@ -120,9 +128,94 @@ NB_MODULE(hyperon_das_node_ext, m) {
       .def("message_factory", &DistributedAlgorithmNode::message_factory);
 
   // StarNode.h bindings
-  nb::class_<StarNode, DistributedAlgorithmNode>(m, "StarNode")
+  nb::class_<StarNode, DistributedAlgorithmNode, StarNodeTrampoline>(m, "StarNode")
       .def(nb::init<const string&, MessageBrokerType>(),
            "node_id"_a, "messaging_backend"_a = MessageBrokerType::GRPC)
       .def(nb::init<const string&, const string&, MessageBrokerType>(),
            "node_id"_a, "server_id"_a, "messaging_backend"_a = MessageBrokerType::GRPC);
+
+    // DASNode.h
+    nb::class_<DASNode, StarNode>(m, "DASNode")
+        .def(nb::init<const string&>(), "node_id"_a)
+        .def(nb::init<const string&, const string&>(), "node_id"_a, "server_id"_a)
+        .def_ro_static("PATTERN_MATCHING_QUERY", &DASNode::PATTERN_MATCHING_QUERY)
+        .def_ro_static("COUNTING_QUERY", &DASNode::COUNTING_QUERY)
+        .def(
+            "pattern_matcher_query",
+            [](DASNode& self,
+               const vector<string>& tokens,
+               const string& context,
+               bool update_attention_broker) -> shared_ptr<RemoteIterator<HandlesAnswer>> {
+                return shared_ptr<RemoteIterator<HandlesAnswer>>(
+                    self.pattern_matcher_query(tokens, context, update_attention_broker));
+            },
+            "tokens"_a,
+            "context"_a = "",
+            "update_attention_broker"_a = false)
+        .def("count_query",
+             &DASNode::count_query,
+             "tokens"_a,
+             "context"_a = "",
+             "update_attention_broker"_a = false)
+        .def("next_query_id", &DASNode::next_query_id)
+        .def("message_factory", &DASNode::message_factory, "command"_a, "args"_a);
+
+
+    // QueryAnswer.h bindings
+    nb::class_<QueryAnswer>(m, "QueryAnswer")
+        .def("tokenize", &QueryAnswer::tokenize)
+        .def("untokenize", &QueryAnswer::untokenize, "tokens"_a)
+        .def("to_string", &QueryAnswer::to_string);
+
+    // HandlesAnswer.h
+    nb::class_<HandlesAnswer, QueryAnswer>(m, "HandlesAnswer")
+        .def(nb::init<>())
+        .def(nb::init<double>(), "importance"_a)
+        .def(
+            "__init__",
+            [](HandlesAnswer& self, const string& handle, double importance) {
+                new (&self) HandlesAnswer(handle.c_str(), importance);
+            },
+            "handle"_a,
+            "importance"_a)
+        .def_prop_ro("handles",
+                     [](const HandlesAnswer& self) -> const vector<string> {
+                         vector<string> handles;
+                         for (size_t i = 0; i < self.handles_size; i++) {
+                             handles.emplace_back(self.handles[i]);
+                         }
+                         return handles;
+                     })
+        .def_ro("handles_size", &HandlesAnswer::handles_size)
+        .def_ro("importance", &HandlesAnswer::importance)
+        .def(
+            "add_handle",
+            [](HandlesAnswer& self, const string& handle) { self.add_handle(handle.c_str()); },
+            "handle"_a)
+        .def(
+            "merge",
+            [](HandlesAnswer& self, const HandlesAnswer& other, bool merge_handles) -> bool {
+                return self.merge((HandlesAnswer*) &other, merge_handles);
+            },
+            "other"_a,
+            "merge_handles"_a)
+        .def_static(
+            "copy",
+            [](const HandlesAnswer& other) -> shared_ptr<HandlesAnswer> {
+                return shared_ptr<HandlesAnswer>(HandlesAnswer::copy((HandlesAnswer*) &other));
+            },
+            // C++ implementation named it `base` instead of `other`, but `other` is more idiomatic
+            "other"_a);
+
+    // RemoteIterator.h
+    nb::class_<RemoteIterator<HandlesAnswer>>(m, "RemoteIterator")
+        .def(nb::init<const string&>(), "local_id"_a)
+        .def_ro("is_terminal", &RemoteIterator<HandlesAnswer>::is_terminal)
+        .def("graceful_shutdown", &RemoteIterator<HandlesAnswer>::graceful_shutdown)
+        .def("setup_buffers", &RemoteIterator<HandlesAnswer>::setup_buffers)
+        .def("finished", &RemoteIterator<HandlesAnswer>::finished)
+        .def("pop", [](RemoteIterator<HandlesAnswer>& self) -> shared_ptr<HandlesAnswer> {
+            return shared_ptr<HandlesAnswer>((HandlesAnswer*) self.pop());
+        });
+
 }
