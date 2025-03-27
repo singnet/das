cd8833a09634dc286f6f8d5f6f8641acbf44fc14
Pedro Costa <pedrobc89@gmail.com>
2025-03-27 20:51:45 +0000

    env-file and simplify cache

--- src/scripts/bazel.sh
+++ src/scripts/bazel.sh
@@ -6,34 +6,20 @@ IMAGE_NAME="das-builder"
 CONTAINER_NAME=${IMAGE_NAME}-container
 BAZEL_CMD="/opt/bazel/bazelisk"
 
-ENV_VARS=$(printenv | sort | awk -F= '{print "--env "$1}')
+ENV_VARS=$(test -f .env && echo "--env-file=.env" || echo "")
 
 # local paths
 LOCAL_WORKDIR=$(pwd)
 LOCAL_BIN_DIR=$LOCAL_WORKDIR/src/bin
-LOCAL_ASPECT_CACHE="$HOME/.cache/das/aspect"
-LOCAL_BAZEL_CACHE="$HOME/.cache/das/bazel"
-LOCAL_BAZELISK_CACHE="$HOME/.cache/das/bazelisk"
-LOCAL_PIPTOOLS_CACHE="$HOME/.cache/das/pip-tools"
-LOCAL_PIP_CACHE="$HOME/.cache/das/pip"
+LOCAL_CACHE="$HOME/.cache/das"
 
-mkdir -p \
-  "$LOCAL_ASPECT_CACHE" \
-  "$LOCAL_BIN_DIR" \
-  "$LOCAL_BAZEL_CACHE" \
-  "$LOCAL_BAZELISK_CACHE" \
-  "$LOCAL_PIPTOOLS_CACHE" \
-  "$LOCAL_PIP_CACHE"
+mkdir -p "$LOCAL_BIN_DIR" "$LOCAL_CACHE" 
 
 # container paths
 CONTAINER_WORKDIR=/opt/das
 CONTAINER_WORKSPACE_DIR=/opt/das/src
 CONTAINER_BIN_DIR=$CONTAINER_WORKSPACE_DIR/bin
-CONTAINER_ASPECT_CACHE=/home/"${USER}"/.cache/aspect
-CONTAINER_BAZEL_CACHE=/home/"${USER}"/.cache/bazel
-CONTAINER_PIP_CACHE=/home/"${USER}"/.cache/pip
-CONTAINER_PIPTOOLS_CACHE=/home/"${USER}"/.cache/pip-tools
-CONTAINER_BAZELISK_CACHE=/home/"${USER}"/.cache/bazelisk
+CONTAINER_CACHE=/home/"${USER}"/.cache
 
 if docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
   echo "Removing existing container: ${CONTAINER_NAME}"
@@ -47,11 +33,7 @@ docker run --rm \
   $ENV_VARS \
   --network=host \
   --volume /etc/passwd:/etc/passwd:ro \
-  --volume "$LOCAL_PIP_CACHE":"$CONTAINER_PIP_CACHE" \
-  --volume "$LOCAL_PIPTOOLS_CACHE":"$CONTAINER_PIPTOOLS_CACHE" \
-  --volume "$LOCAL_ASPECT_CACHE":"$CONTAINER_ASPECT_CACHE" \
-  --volume "$LOCAL_BAZEL_CACHE":"$CONTAINER_BAZEL_CACHE" \
-  --volume "$LOCAL_BAZELISK_CACHE":"$CONTAINER_BAZELISK_CACHE" \
+  --volume "$LOCAL_CACHE":"$CONTAINER_CACHE" \
   --volume "$LOCAL_WORKDIR":"$CONTAINER_WORKDIR" \
   --workdir "$CONTAINER_WORKSPACE_DIR" \
   --entrypoint "$BAZEL_CMD" \
