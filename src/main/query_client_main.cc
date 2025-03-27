9e22f2aed34cf24e43ff51a0cb219ccdd188d476
Artur Gontijo <arturgontijo@gmail.com>
2025-03-27 17:31:25 -0300

    Add update_attention_broker param.

--- src/main/query_client_main.cc
+++ src/main/query_client_main.cc
@@ -21,27 +21,28 @@ void ctrl_c_handler(int) {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 4) {
+    if (argc < 5) {
         cerr << "Usage: " << argv[0]
-             << " CLIENT_HOST:CLIENT_PORT SERVER_HOST:SERVER_PORT QUERY_TOKEN+ (hosts are supposed to "
-                "be public IPs or known hostnames)"
+             << " CLIENT_HOST:CLIENT_PORT SERVER_HOST:SERVER_PORT UPDATE_ATTENTION_BROKER QUERY_TOKEN+ "
+                "(hosts are supposed to be public IPs or known hostnames)"
              << endl;
         exit(1);
     }
 
     string client_id = string(argv[1]);
     string server_id = string(argv[2]);
+    bool update_attention_broker = (string(argv[3]) == "true" || string(argv[3]) == "1");
 
     signal(SIGINT, &ctrl_c_handler);
     vector<string> query;
-    for (int i = 3; i < argc; i++) {
+    for (int i = 4; i < argc; i++) {
         query.push_back(argv[i]);
     }
 
     DASNode client(client_id, server_id);
     QueryAnswer* query_answer;
     unsigned int count = 0;
-    RemoteIterator<HandlesAnswer>* response = client.pattern_matcher_query(query);
+    RemoteIterator<HandlesAnswer>* response = client.pattern_matcher_query(query, "", update_attention_broker);
     while (!response->finished()) {
         if ((query_answer = response->pop()) == NULL) {
             Utils::sleep();
