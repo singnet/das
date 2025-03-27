b6edcda35cac24b5d9595766a85bdcd900723c53
Pedro Costa <pedrobc89@gmail.com>
2025-03-27 20:55:33 +0000

    env-file to container_tty

--- src/scripts/container_tty.sh
+++ src/scripts/container_tty.sh
@@ -2,6 +2,8 @@
 
 CONTAINER_NAME="das-builder-bash"
 
+ENV_VARS=$(test -f .env && echo "--env-file=.env" || echo "")
+
 PARAMS="bash"
 
 if [ $# -gt 0 ]; then
@@ -13,9 +15,11 @@ if docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
   docker rm -f "${CONTAINER_NAME}"
 fi
 
+
 docker run --rm \
     --net="host" \
     --name=$CONTAINER_NAME \
+    $ENV_VARS \
     --volume /tmp:/tmp \
     --volume .:/opt/das \
     -it das-builder \
