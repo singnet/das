c7cc90cb438bc5d9165274d881f3b4fc618e5b7d
Edgar Brissow <eddiebrissow@gmail.com>
2025-03-28 16:18:21 -0300

    Using das_node StarNode

--- src/das_agent/das_agent_node.py
+++ src/das_agent/das_agent_node.py
@@ -3,6 +3,7 @@ from hyperon_das_node.hyperon_das_node_ext import (
     MessageBrokerType,
     DistributedAlgorithmNode,
     Message,
+    StarNode,
 )
 from typing import List
 from queue import Queue
@@ -18,46 +19,12 @@ class DASLinkCreateMessage(Message):
         node.add_request(self.message)
 
 
-class StarNode(DistributedAlgorithmNode):
-    is_server: bool
-    server_id: str
-
-    def __init__(
-        self,
-        node_id: str,
-        server_id: str = None,
-        messaging_backend: MessageBrokerType = MessageBrokerType.GRPC,
-    ):
-        # Call the parent constructor (DistributedAlgorithmNode)
-        super().__init__(node_id, LeadershipBrokerType.SINGLE_MASTER_SERVER, messaging_backend)
-        self.node_id = node_id
-        if server_id:
-            # If server_id is provided, this is a client node
-            self.server_id = server_id
-            self.is_server = False
-            self.add_peer(server_id)
-        else:
-            # If no server_id, this is a server node
-            self.is_server = True
-
-        # Join the network regardless of server/client
-        self.join_network()
-
-    def node_joined_network(self, node_id: str):
-        if self.is_server:
-            print(f"Server {self.node_id} joined by {node_id}")
-            self.add_peer(node_id)
-
-    def cast_leadership_vote(self) -> str:
-        if self.is_server:
-            return self.node_id
-        else:
-            return self.server_id
-
-
 class DASAgentNode(StarNode):
     def __init__(self, node_id: str = None, server_id: str = None):
-        super().__init__(node_id, server_id)
+        if server_id is None:
+            super().__init__(node_id)
+        else:
+            super().__init__(node_id, server_id)
         self.requests = Queue()
 
     def message_factory(self, command: str, args: list) -> Message:
