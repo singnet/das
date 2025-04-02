f25eb044703a961b8724f8b6fb4392f8bcb8730f
Pedro Costa <pedrobc89@gmail.com>
2025-04-02 13:54:40 +0000

    fixed typo

--- src/scripts/bazel_cmd.sh
+++ src/scripts/bazel_cmd.sh
@@ -4,6 +4,6 @@ set -exou pipefail
 
 CMD="/opt/bazel/bazelisk"
 
-ARGS=$([ ${BAZEL_JOBS:-x} != x ] && echo --jobs=${BAZEL_JOBS})
+ARGS=$([ ${BAZEL_JOBS:-} != x ] && echo --jobs=${BAZEL_JOBS})
 
 ${CMD} ${ARGS} ${@}
