1fba7482d5664688615793cf07aad1575317bd7f
Angelo Probst <33906777+angeloprobst@users.noreply.github.com>
2025-05-15 16:44:14 -0300

    [#392] fixing variables matching (#398)
    * wip - it seems that there is no way to know the inner template vars
    
    * wip
    
    * supporting inner LT2
    
    * fixing bug
    
    * fixing but
    
    * commenting out cout
    
    * efficiency improvements
    
    * removing couts
    
    * adding doxygen
    
    * efficiency improvements
    
    * adding comment
    
    * code cleanup
    
    * code cleanup
    
    * code cleanup, comments, final version

--- src/agents/query_engine/query_element/LinkTemplate2.h
+++ src/agents/query_engine/query_element/LinkTemplate2.h
@@ -11,6 +11,7 @@
 #include "AtomDBAPITypes.h"
 #include "AtomDBSingleton.h"
 #include "Iterator.h"
+#include "LinkTemplate.h"
 #include "QueryAnswer.h"
 #include "QueryNode.h"
 #include "SharedQueue.h"
@@ -41,25 +42,40 @@ class LinkTemplate2 : public Source {
         this->db = AtomDBSingleton::get_instance();
         this->handle_keys[0] =
             (wildcard_flag ? (char*) AtomDB::WILDCARD.c_str() : named_type_hash((char*) type.c_str()));
+        this->keys_template[0] = this->handle_keys[0];  // to be used in the `get_link_handle` method
         for (unsigned int i = 1; i <= ARITY; i++) {
+            this->keys_template[i] = NULL;
+            if (this->target_template[i - 1]->is_operator) {
+                Utils::error("LinkTemplate2 does not support operators.");
+            }
             // It's safe to get stored shared_ptr's raw pointer here because handle_keys[]
             // is used solely in this scope so it's guaranteed that handle will not be freed.
             if (this->target_template[i - 1]->is_terminal) {
+                auto terminal = dynamic_pointer_cast<Terminal>(this->target_template[i - 1]);
+                if (terminal->is_variable) {
+                    Utils::error("LinkTemplate2 does not support variables.");
+                }
                 this->handle_keys[i] =
                     dynamic_pointer_cast<Terminal>(this->target_template[i - 1])->handle.get();
+                this->keys_template[i] =  // to be used in the `get_link_handle` method
+                    this->handle_keys[i];
             } else {
                 this->handle_keys[i] = (char*) AtomDB::WILDCARD.c_str();
                 this->inner_template.push_back(this->target_template[i - 1]);
+                this->inner_positions.push_back(i);  // to be used in the `get_link_handle` method
             }
         }
-        if (this->inner_template.size() == 0) {
-            Utils::error("LinkTemplate2: No inner template.");
-        }
+
+        this->validate_inner_template();
+
         this->handle =
             shared_ptr<char>(composite_hash(this->handle_keys, ARITY + 1), default_delete<char[]>());
-        if (!wildcard_flag) {
+
+        // moved to the destructor
+        /*if (!wildcard_flag) {
             free(this->handle_keys[0]);
-        }
+        }*/
+
         // This is correct. id is not necessarily a handle but an identifier. It just happens
         // that we want the string for this identifier to be the same as the string representing
         // the handle.
@@ -67,11 +83,65 @@ class LinkTemplate2 : public Source {
         LOG_INFO("LinkTemplate2 " << this->to_string());
     }
 
+    void validate_inner_template() {
+        if (this->inner_template.size() == 0) {
+            Utils::error("LinkTemplate2: No inner template.");
+        }
+        vector<pair<size_t, string>> variables;
+        bool is_lt2 = false;
+        for (auto& inner_template : this->inner_template) {
+            is_lt2 = false;
+            variables = {};
+            switch (inner_template->arity) {
+                case 1:
+                    if (auto lt = dynamic_pointer_cast<LinkTemplate<1>>(inner_template)) {
+                        variables = lt->get_variables();
+                    } else if (auto lt2 = dynamic_pointer_cast<LinkTemplate2<1>>(inner_template)) {
+                        is_lt2 = true;
+                    }
+                    break;
+                case 2:
+                    if (auto lt = dynamic_pointer_cast<LinkTemplate<2>>(inner_template)) {
+                        variables = lt->get_variables();
+                    } else if (auto lt2 = dynamic_pointer_cast<LinkTemplate2<2>>(inner_template)) {
+                        is_lt2 = true;
+                    }
+                    break;
+                case 3:
+                    if (auto lt = dynamic_pointer_cast<LinkTemplate<3>>(inner_template)) {
+                        variables = lt->get_variables();
+                    } else if (auto lt2 = dynamic_pointer_cast<LinkTemplate2<3>>(inner_template)) {
+                        is_lt2 = true;
+                    }
+                    break;
+                case 4:
+                    if (auto lt = dynamic_pointer_cast<LinkTemplate<4>>(inner_template)) {
+                        variables = lt->get_variables();
+                    } else if (auto lt2 = dynamic_pointer_cast<LinkTemplate2<4>>(inner_template)) {
+                        is_lt2 = true;
+                    }
+                    break;
+                default:
+                    Utils::error("Invalid number of inner templates in LinkTemplate2.");
+            }
+            if (is_lt2) {
+                Utils::error("LinkTemplate2 does not support LinkTemplate2 as inner templates.");
+            }
+            if (variables.size() == 0) {
+                Utils::error("LinkTemplate2 does not support inner templates without variables.");
+            }
+            this->inner_template_variables.push_back(variables);
+        }
+    }
+
     /**
      * Destructor.
      */
     virtual ~LinkTemplate2() {
         this->graceful_shutdown();
+        if (this->handle_keys[0] != (char*) AtomDB::WILDCARD.c_str()) {
+            free(this->handle_keys[0]);
+        }
         this->inner_template.clear();
         this->inner_template_iterator.reset();
     }
@@ -160,31 +230,68 @@ class LinkTemplate2 : public Source {
             new thread(&LinkTemplate2::inner_templates_processor_method, this);
     }
 
+    vector<pair<size_t, string>> get_variables() {
+        vector<pair<size_t, string>> variables;
+        for (unsigned int i = 0; i < this->arity; i++) {
+            if (this->target_template[i]->is_terminal) {
+                auto terminal = dynamic_pointer_cast<Terminal>(this->target_template[i]);
+                if (terminal->is_variable) {
+                    variables.push_back({i, terminal->name});
+                }
+            }
+        }
+        return variables;
+    }
+
    private:
     // --------------------------------------------------------------------------------------------
     // Private methods and attributes
 
+    /**
+     * Generates a link handle by matching data from QueryAnswer with the LinkTemplate2 structure.
+     * This function iterates over the inner templates, extracting variables and identifying matches
+     * between the template's targets and the QueryAnswer's assignment. If a match is found, it
+     * assigns the query answer's handle to the template's key. The resulting composite hash of keys
+     * is returned as the link handle. If no match is found for a template, the function returns NULL.
+     *
+     * @note This function is not thread-safe.
+     * @note Caller is responsible for freeing the returned char pointer.
+     *
+     * @param query_answer Pointer to a QueryAnswer object containing handles and variable assignments.
+     * @return A char pointer representing the composite hash link handle, or NULL if no match exists.
+     */
     char* get_link_handle(QueryAnswer* query_answer) {
-        char* handle_keys[ARITY];
-        handle_keys[0] = named_type_hash((char*) this->type.c_str());
-        auto qa_handles_size = query_answer->handles_size;
-        size_t qa_handles_index = 0;
-        for (unsigned int i = 0; i < ARITY; i++) {
-            if (this->target_template[i]->is_terminal) {
-                handle_keys[i + 1] =
-                    dynamic_pointer_cast<Terminal>(this->target_template[i])->handle.get();
-            } else {
-                // QueryAnswer doesn't have one or more handles for this template
-                if (qa_handles_index == qa_handles_size) {
-                    free(handle_keys[0]);
-                    return NULL;
+        size_t i = 0;  // template vars index
+        shared_ptr<atomdb::atomdb_api_types::AtomDocument> atom;
+        for (auto inner_position : this->inner_positions) {
+            if (i >= this->inner_template.size()) Utils::error("Invalid inner template index.");
+            this->keys_template[inner_position] = NULL;
+            // clang-format off
+            for (
+                size_t qa_handles_index = 0;
+                qa_handles_index < query_answer->handles_size;
+                qa_handles_index++
+            ) {
+                // clang-format on
+                auto qa_handle = query_answer->handles[qa_handles_index];
+                atom = this->db->get_atom_document(qa_handle);
+                if (!atom->contains("targets")) continue;
+                for (auto& [index, name] : this->inner_template_variables[i]) {
+                    auto target_handle = atom->get("targets", index);
+                    if (!target_handle) continue;
+                    auto assignment_handle = query_answer->assignment.get(name.c_str());
+                    if (!assignment_handle) continue;
+                    if (strcmp(target_handle, assignment_handle) == 0) {
+                        this->keys_template[inner_position] = (char*) qa_handle;
+                        break;
+                    }
                 }
-                handle_keys[i + 1] = (char*) query_answer->handles[qa_handles_index++];
+                if (this->keys_template[inner_position] != NULL) break;
             }
+            if (this->keys_template[inner_position] == NULL) return NULL;
+            i++;
         }
-        auto hash = composite_hash(handle_keys, ARITY + 1);
-        free(handle_keys[0]);
-        return hash;
+        return composite_hash(this->keys_template, ARITY + 1);
     }
 
     void inner_templates_processor_method() {
@@ -209,13 +316,9 @@ class LinkTemplate2 : public Source {
                 this->link_handles.push_back(link_handle);
                 this->link_handles_to_query_answers[link_handle] = query_answer;
                 free(link_handle);
-                if (this->link_handles.size() >= DB_LINK_HANDLES_BATCH_SIZE) {
-                    break;
-                }
+                if (this->link_handles.size() >= DB_LINK_HANDLES_BATCH_SIZE) break;
             }
-            if (this->link_handles.size() == 0) {
-                continue;
-            }
+            if (this->link_handles.size() == 0) continue;
             auto existing_handles = this->db->links_exist(this->link_handles);
             for (auto& link_handle : existing_handles) {
                 query_answer = this->link_handles_to_query_answers[link_handle];
@@ -224,9 +327,7 @@ class LinkTemplate2 : public Source {
                 this->output_buffer->add_query_answer(query_answer);
                 this->link_handles_to_query_answers.erase(link_handle);
             }
-            for (auto& [link_handle, query_answer] : this->link_handles_to_query_answers) {
-                delete query_answer;
-            }
+            for (auto& [_, query_answer] : this->link_handles_to_query_answers) delete query_answer;
         }
         this->output_buffer->query_answers_finished();
     }
@@ -253,7 +354,6 @@ class LinkTemplate2 : public Source {
 
     string type;
     array<shared_ptr<QueryElement>, ARITY> target_template;
-    unsigned int arity;
     shared_ptr<char> handle;
     char* handle_keys[ARITY + 1];
     vector<shared_ptr<QueryElement>> inner_template;
@@ -262,6 +362,12 @@ class LinkTemplate2 : public Source {
     shared_ptr<AtomDB> db;
     vector<string> link_handles;
     unordered_map<string, QueryAnswer*> link_handles_to_query_answers;
+
+    /* ∨∨∨ to be used in the `get_link_handle` method ∨∨∨ */
+    char* keys_template[ARITY + 1];
+    vector<size_t> inner_positions;
+    vector<vector<pair<size_t, string>>> inner_template_variables;
+    /* ∧∧∧ to be used in the `get_link_handle` method ∧∧∧ */
 };
 
 }  // namespace query_element
