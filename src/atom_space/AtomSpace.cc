986004035f47c33694ea3e33ad4d4c6c695a58d4
Angelo Probst <angelo.probst@gmail.com>
2025-06-03 20:56:36 -0300

    added separator

--- src/atom_space/AtomSpace.cc
+++ src/atom_space/AtomSpace.cc
@@ -5,6 +5,7 @@ using namespace query_engine;
 
 namespace atomspace {
 
+// -------------------------------------------------------------------------------------------------
 const Atom* AtomSpace::get_atom(const char* handle, Scope scope) {
     if (scope == LOCAL_ONLY || scope == LOCAL_AND_REMOTE) {
         auto atom = this->handle_trie->lookup(handle);
@@ -14,18 +15,21 @@ const Atom* AtomSpace::get_atom(const char* handle, Scope scope) {
     return NULL;
 }
 
+// -------------------------------------------------------------------------------------------------
 const Node* AtomSpace::get_node(const string& type, const string& name, Scope scope) {
     auto node = this->get_atom(Node(type, name).compute_handle());
     if (node) return dynamic_cast<const Node*>(node);
     return NULL;
 }
 
+// -------------------------------------------------------------------------------------------------
 const Link* AtomSpace::get_link(const string& type, const vector<Atom*>& targets, Scope scope) {
     auto link = this->get_atom(Link(type, targets).compute_handle());
     if (link) return dynamic_cast<const Link*>(link);
     return NULL;
 }
 
+// -------------------------------------------------------------------------------------------------
 shared_ptr<PatternMatchingQueryProxy> AtomSpace::pattern_matching_query(const vector<string>& query,
                                                                         unsigned int answers_count,
                                                                         const string& context,
@@ -40,6 +44,7 @@ shared_ptr<PatternMatchingQueryProxy> AtomSpace::pattern_matching_query(const ve
     return proxy;
 }
 
+// -------------------------------------------------------------------------------------------------
 unsigned int AtomSpace::pattern_matching_count(const vector<string>& query,
                                                const string& context,
                                                bool unique_assignment,
@@ -53,10 +58,12 @@ unsigned int AtomSpace::pattern_matching_count(const vector<string>& query,
     return proxy->get_count();
 }
 
+// -------------------------------------------------------------------------------------------------
 void AtomSpace::pattern_matching_fetch(const vector<string>& query, unsigned int answers_count) {
     // TODO: Implement the fetch logic once the server-side supports it.
 }
 
+// -------------------------------------------------------------------------------------------------
 char* AtomSpace::add_node(const string& type, const string& name) {
     Node* node = new Node(type, name);
     char* handle = node->compute_handle();
@@ -64,6 +71,7 @@ char* AtomSpace::add_node(const string& type, const string& name) {
     return handle;
 }
 
+// -------------------------------------------------------------------------------------------------
 char* AtomSpace::add_link(const string& type, const vector<Atom*>& targets) {
     Link* link = new Link(type, targets);
     char* handle = link->compute_handle();
@@ -71,6 +79,7 @@ char* AtomSpace::add_link(const string& type, const vector<Atom*>& targets) {
     return handle;
 }
 
+// -------------------------------------------------------------------------------------------------
 void AtomSpace::commit_changes(Scope scope) {
     // TODO: Implement commit logic once the server-side supports it.
 }
