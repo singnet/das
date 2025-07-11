77e319a6bd76ecd8ead2885914c5866cdf14bd03
Artur Gontijo <arturgontijo@gmail.com>
2025-07-11 11:51:52 -0300

    Remove strdup().

--- src/hasher/expression_hasher.cc
+++ src/hasher/expression_hasher.cc
@@ -7,37 +7,37 @@
 #include "mbedtls/md5.h"
 
 char* compute_hash(char* input) {
-    unsigned char MD5_BUFFER[16];
-    char HASH[HANDLE_HASH_SIZE];
+    unsigned char md5_buffer[16];
+    char* hash = new char[HANDLE_HASH_SIZE];
 
     mbedtls_md5_context context;
     mbedtls_md5_init(&context);
     mbedtls_md5_starts(&context);
     mbedtls_md5_update(&context, (const unsigned char*) input, strlen(input));
-    mbedtls_md5_finish(&context, MD5_BUFFER);
+    mbedtls_md5_finish(&context, md5_buffer);
     mbedtls_md5_free(&context);
     for (unsigned int i = 0; i < 16; i++) {
-        sprintf((char*) ((unsigned long) HASH + 2 * i), "%02x", MD5_BUFFER[i]);
+        sprintf((char*) ((unsigned long) hash + 2 * i), "%02x", md5_buffer[i]);
     }
-    HASH[32] = '\0';
-    return strdup(HASH);
+    hash[32] = '\0';
+    return hash;
 }
 
 char* named_type_hash(char* name) { return compute_hash(name); }
 
 char* terminal_hash(char* type, char* name) {
-    char HASHABLE_STRING[MAX_HASHABLE_STRING_SIZE];
+    char hashable_string[MAX_HASHABLE_STRING_SIZE];
 
     if (strlen(type) + strlen(name) >= MAX_HASHABLE_STRING_SIZE) {
         fprintf(stderr, "Invalid (too large) terminal name");
         exit(1);
     }
-    sprintf(HASHABLE_STRING, "%s%c%s", type, JOINING_CHAR, name);
-    return compute_hash(HASHABLE_STRING);
+    sprintf(hashable_string, "%s%c%s", type, JOINING_CHAR, name);
+    return compute_hash(hashable_string);
 }
 
 char* composite_hash(char** elements, unsigned int nelements) {
-    char HASHABLE_STRING[MAX_HASHABLE_STRING_SIZE];
+    char hashable_string[MAX_HASHABLE_STRING_SIZE];
     unsigned int total_size = 0;
     unsigned int element_size[nelements];
 
@@ -58,15 +58,15 @@ char* composite_hash(char** elements, unsigned int nelements) {
     unsigned long cursor = 0;
     for (unsigned int i = 0; i < nelements; i++) {
         if (i == (nelements - 1)) {
-            strcpy((char*) (HASHABLE_STRING + cursor), elements[i]);
+            strcpy((char*) (hashable_string + cursor), elements[i]);
         } else {
-            sprintf((char*) (HASHABLE_STRING + cursor), "%s%c", elements[i], JOINING_CHAR);
+            sprintf((char*) (hashable_string + cursor), "%s%c", elements[i], JOINING_CHAR);
             cursor += 1;
         }
         cursor += element_size[i];
     }
 
-    return compute_hash(HASHABLE_STRING);
+    return compute_hash(hashable_string);
 }
 
 char* expression_hash(char* type_hash, char** elements, unsigned int nelements) {
