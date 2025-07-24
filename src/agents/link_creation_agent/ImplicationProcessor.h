725b2f1203c4b8d1b99d0ab9fe25d2786ae5929d
eddiebrissow <eddiebrissow@gmail.com>
2025-07-24 14:23:58 -0300

    [#572] AtomDB support to LCA (#585)
    * merge
    
    * Working version
    
    * cleanup ok
    
    * Formating
    
    * Using tokens to build LinkSchema
    
    * Using tokens to build LinkSchema

--- src/agents/link_creation_agent/ImplicationProcessor.h
+++ src/agents/link_creation_agent/ImplicationProcessor.h
@@ -3,30 +3,22 @@
  */
 
 #pragma once
-#include "LCALink.h"
+#include "Link.h"
 #include "LinkProcessor.h"
+#include "LinkSchema.h"
 
 using namespace query_engine;
+using namespace atoms;
 using namespace std;
 
 namespace link_creation_agent {
 class ImplicationProcessor : public LinkProcessor {
    public:
     ImplicationProcessor();
-    vector<vector<string>> process(shared_ptr<QueryAnswer> query_answer,
-                                   optional<vector<string>> extra_params = nullopt) override;
+    vector<shared_ptr<Link>> process_query(shared_ptr<QueryAnswer> query_answer,
+                                           optional<vector<string>> extra_params = nullopt) override;
 
-   private:
-    static vector<string> pattern_template_1;
-    static vector<string> pattern_template_2;
-    static vector<string> ss_template_1;
-    static vector<string> ss_template_2;
-    static vector<string> ss_template_3;
-    static vector<string> get_pattern_query(const vector<string>& p);
-    static vector<string> get_satisfying_set_query(const vector<string>& p1, const vector<string>& p2);
-    static vector<string> get_tokenized_atom(const string& handle);
-    static LCALink build_link(const string& link_type,
-                              vector<LinkTargetTypes> targets,
-                              vector<CustomField> custom_fields);
+    static LinkSchema build_pattern_query(const string& handle);
+    static LinkSchema build_satisfying_set_query(const string& p1_handle, const string& p2_handle);
 };
 }  // namespace link_creation_agent
