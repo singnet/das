b9308ec99246feef9a20908a28d4b1489c71e61b
rafael.levi <rafael.levi@singularitynet.io>
2025-09-02 10:07:40 -0300

    das-118: add combined_report param to coverage

--- src/scripts/bazel_coverage_check.sh
+++ src/scripts/bazel_coverage_check.sh
@@ -6,15 +6,20 @@ SCRIPT_DIR="$(dirname "$0")"
 REPORT="bazel-out/_coverage/_coverage_report.dat"
 THRESHOLD=70
 
+bash "$SCRIPT_DIR/bazel_exec.sh" coverage --combined_report=lcov --keep_going --show_progress //tests/...
 
-bash "$SCRIPT_DIR/bazel_exec.sh" coverage --keep_going --show_progress //tests/...
+if [[ -z "$REPORT" ]]; then
+  echo "[Error] coverage file not found!"
+  exit 1
+fi
+
 
 COVERAGE=$(lcov --summary "$REPORT" | grep "lines......" | awk '{print $2}' | sed 's/%//')
 
 echo "Line coverage: $COVERAGE%"
 
 if (( $(echo "$COVERAGE < $THRESHOLD" | bc -l) )); then
-    echo "Error: line coverage $COVERAGE% is below threshold of $THRESHOLD%"
+    echo "[Error]: line coverage $COVERAGE% is below threshold of $THRESHOLD%"
     exit 1
 fi
 
