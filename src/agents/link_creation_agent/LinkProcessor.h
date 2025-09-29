91f73375d3ae404d33a308795c27405d7bfe84f4
eddiebrissow <eddiebrissow@gmail.com>
2025-09-29 10:43:16 -0300

    [#632] LCA and Inference tunning/improvements (#780)
    * New scenario
    
    * More setup script options
    
    * Minor fixes, a new scenarios and a new script to join the databases (#776)
    
    Co-authored-by: Andre Senna <“andre.senna@gmail.com”>
    
    * LCA and Inference Agent improvements
    
    * Format
    
    * Fixed tests
    
    ---------
    
    Co-authored-by: Andre Luiz de Senna <andre.senna@gmail.com>
    Co-authored-by: Andre Senna <“andre.senna@gmail.com”>

--- src/agents/link_creation_agent/LinkProcessor.h
+++ src/agents/link_creation_agent/LinkProcessor.h
@@ -13,11 +13,14 @@
 
 #include "Link.h"
 #define LOG_LEVEL DEBUG_LEVEL
+#include "AtomDBSingleton.h"
 #include "Logger.h"
 #include "PatternMatchingQueryProxy.h"
 #include "QueryAnswer.h"
 #include "ServiceBusSingleton.h"
 #include "Utils.h"
+
+#define TARGETS_SIZE 2
 
 using namespace atoms;
 using namespace query_engine;
@@ -36,6 +39,7 @@ class LinkProcessor {
         // Utils::sleep(500);  // TODO fix this, waiting to delete to not crash
         return count;
     }
+
     static int _count_query(vector<string>& query, string& context, bool is_unique_assignment = true) {
         try {
             shared_ptr<PatternMatchingQueryProxy> query_count_proxy =
@@ -50,6 +54,91 @@ class LinkProcessor {
         } catch (const exception& e) {
             LOG_ERROR("Exception: " << e.what());
             return 0;
+        }
+    }
+
+    static void insert_or_update(map<string, double>& count_map, const string& key, double value) {
+        auto iterator = count_map.find(key);
+        if (iterator == count_map.end()) {
+            count_map[key] = value;
+        } else {
+            if (value > iterator->second) {
+                count_map[key] = value;
+            }
+        }
+    }
+
+    static void compute_counts(const vector<vector<string>>& queries,
+                               const string& context,
+                               const QueryAnswerElement& target_element,
+                               vector<double>& counts,
+                               double& count_intersection,
+                               double& count_union) {
+        if (queries.size() == 0 || queries.size() > TARGETS_SIZE) {
+            counts = {};
+            count_intersection = 0;
+            count_union = 0;
+            Utils::error("Invalid number of queries for compute_counts");
+        }
+        vector<shared_ptr<PatternMatchingQueryProxy>> query_proxies;
+        for (const auto& query : queries) {
+            counts.push_back(0);
+            try {
+                shared_ptr<PatternMatchingQueryProxy> query_count_proxy =
+                    make_shared<PatternMatchingQueryProxy>(query, context);
+                query_count_proxy->parameters[BaseQueryProxy::UNIQUE_ASSIGNMENT_FLAG] = true;
+                query_proxies.push_back(query_count_proxy);
+            } catch (const exception& e) {
+                Utils::error("Exception: " + string(e.what()));
+            }
+        }
+
+        map<string, double> count_map[TARGETS_SIZE];
+        map<string, double> count_map_union;
+        map<string, double> count_map_intersection;
+        shared_ptr<QueryAnswer> query_answer;
+        auto db = AtomDBSingleton::get_instance();
+        double d;
+        string handle;
+        for (unsigned int i = 0; i < TARGETS_SIZE; i++) {
+            ServiceBusSingleton::get_instance()->issue_bus_command(query_proxies[i]);
+            while (!query_proxies[i]->finished()) {
+                if ((query_answer = query_proxies[i]->pop()) == NULL) {
+                    Utils::sleep(20);
+                } else {
+                    if (query_answer->handles.size() == 1) {
+                        d = 1.0;
+                    } else {
+                        string link_handle = query_answer->handles[1];
+                        auto link = db->get_atom(link_handle);
+                        d = link->custom_attributes.get<double>("strength");
+                    }
+                    handle = query_answer->get(target_element);
+                    insert_or_update(count_map[i], handle, d);
+                }
+            }
+        }
+        count_map_union = count_map[0];
+        for (auto pair : count_map[1]) {
+            auto iterator = count_map[0].find(pair.first);
+            if (iterator == count_map[0].end()) {
+                insert_or_update(count_map_union, pair.first, pair.second);
+            } else {
+                insert_or_update(count_map_union, pair.first, pair.second);
+                insert_or_update(count_map_intersection, pair.first, pair.second);
+            }
+        }
+
+        for (auto pair : count_map_intersection) {
+            count_intersection += pair.second;
+        }
+        for (auto pair : count_map_union) {
+            count_union += pair.second;
+        }
+        for (int i = 0; i < TARGETS_SIZE; i++) {
+            for (auto pair : count_map[i]) {
+                counts[i] += pair.second;
+            }
         }
     }
 
