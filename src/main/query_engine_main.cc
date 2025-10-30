f3ea3b07582988cbe59c22a1dee4339d4a572429
Andre Senna <andre.senna@gmail.com>
2025-10-30 14:46:56 -0300

    Added a cmd line parameter to flag the use of MorkDB

--- src/main/query_engine_main.cc
+++ src/main/query_engine_main.cc
@@ -25,9 +25,15 @@ void ctrl_c_handler(int) {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 4) {
-        cerr << "Usage: " << argv[0] << " <hostname:port> <start_port:end_port> <AB_ip:AB_port>" << endl;
+    if ((argc != 4) && (argc != 5)) {
+        cerr << "Usage: " << argv[0] << " <hostname:port> <start_port:end_port> <AB_ip:AB_port> [--use-mork]" << endl;
         exit(1);
+    }
+
+    if ((argc == 5) && (string(argv[4]) == string("--use-mork"))) {
+        AtomDBSingleton::init(atomdb_api_types::ATOMDB_TYPE::MORKDB);
+    } else {
+        AtomDBSingleton::init();
     }
 
     std::string server_id = string(argv[1]);
@@ -37,7 +43,6 @@ int main(int argc, char* argv[]) {
     LOG_INFO("Starting query engine server with id: " + server_id);
     signal(SIGINT, &ctrl_c_handler);
     signal(SIGTERM, &ctrl_c_handler);
-    AtomDBSingleton::init();
     ServiceBusSingleton::init(server_id, "", ports_range.first, ports_range.second);
     shared_ptr<ServiceBus> service_bus = ServiceBusSingleton::get_instance();
     service_bus->register_processor(make_shared<PatternMatchingQueryProcessor>());
