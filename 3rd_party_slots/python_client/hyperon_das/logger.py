ca7edad5e912bf8d9bd9e17b2ebf88b0b532f706
Marco Capozzoli <marcocapozzoli90@gmail.com>
2025-11-12 14:34:52 -0300

    Remove environment variable

--- 3rd_party_slots/python_client/hyperon_das/logger.py
+++ 3rd_party_slots/python_client/hyperon_das/logger.py
@@ -1,6 +1,5 @@
 import logging
 import sys
-from os import environ
 
 RESET = "\033[0m"
 COLOR_MAP = {
@@ -25,23 +24,7 @@ date_format = "%Y-%m-%d %H:%M:%S"
 
 log = logging.getLogger("python_client")
 
-if level := environ.get("LOG_LEVEL"):
-    if level == 'DEBUG':
-        level = logging.DEBUG
-    elif level == 'INFO':
-        level = logging.INFO
-    elif level == 'WARNING':
-        level = logging.WARNING
-    elif level == 'ERROR':
-        level = logging.ERROR
-    elif level == 'CRITICAL':
-        level = logging.CRITICAL
-    else:
-        level = logging.INFO
-else:
-    level = logging.INFO
-
-log.setLevel(level)
+log.setLevel(logging.INFO)
 
 handler = logging.StreamHandler(sys.stdout)
 handler.setFormatter(ColoredFormatter(fmt=log_format, datefmt=date_format))
