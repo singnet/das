dd265eb0fc557c8c89ea6de2692c1b37ddde448d
Marco Capozzoli <marcocapozzoli90@gmail.com>
2025-11-12 14:36:24 -0300

    Remove environment variable

--- 3rd_party_slots/python_client/hyperon_das/logger.py
+++ 3rd_party_slots/python_client/hyperon_das/logger.py
@@ -23,7 +23,6 @@ log_format = "%(asctime)s | [%(levelname)s] | %(message)s"
 date_format = "%Y-%m-%d %H:%M:%S"
 
 log = logging.getLogger("python_client")
-
 log.setLevel(logging.INFO)
 
 handler = logging.StreamHandler(sys.stdout)
