935e2571d20baaca73eb67562070d735ecaa986a
Edgar Brissow <eddiebrissow@gmail.com>
2025-11-27 16:57:12 -0300

    Added thread processing

--- src/agents/atomdb_broker/AtomDBProxy.cc
+++ src/agents/atomdb_broker/AtomDBProxy.cc
@@ -19,10 +19,12 @@ using namespace std;
 // -------------------------------------------------------------------------------------------------
 // Static constants
 
-const size_t AtomDBProxy::BATCH_SIZE = 1000;
+const size_t AtomDBProxy::BATCH_SIZE = 2000;
 const size_t AtomDBProxy::COMMAND_SIZE_LIMIT = 50;
-const static string NODE = "NODE";;
-const static string LINK = "LINK";
+int AtomDBProxy::THREAD_POOL_SIZE = 10;
+size_t AtomDBProxy::MAX_PENDING_ATOMS = 20000;
+string AtomDBProxy::NODE = "NODE";
+string AtomDBProxy::LINK = "LINK";
 
 // Proxy Commands
 string AtomDBProxy::ADD_ATOMS = "add_atoms";
@@ -35,12 +37,24 @@ string AtomDBProxy::END_STREAM = "end_stream";
 AtomDBProxy::AtomDBProxy() : BaseProxy() {
     this->command = ServiceBus::ATOMDB;
     this->atomdb = AtomDBSingleton::get_instance();
-    this->processing_buffer = false;
+    this->is_processing_buffer = false;
+    this->processing_queue = new SharedQueue(AtomDBProxy::MAX_PENDING_ATOMS * 2);
+    this->thread_pool = new ThreadPool(AtomDBProxy::THREAD_POOL_SIZE);
+    this->processing_thread = thread(&AtomDBProxy::process_atom_batches, this);
 }
 
 AtomDBProxy::~AtomDBProxy() {
-    LOG_INFO("Shutdown AtomDBProxy...");
+    LOG_DEBUG("Shuting down AtomDBProxy, waiting for pending atoms to be processed...");
+    while (!this->processing_queue->empty()) {
+        Utils::sleep(100);
+    }
+    this->thread_pool->wait();
+    this->stop_processing = true;
+    this->processing_thread.join();
+    delete this->processing_queue;
+    delete this->thread_pool;
     this->abort();
+    LOG_DEBUG("AtomDBProxy shut down complete.");
 }
 
 // -------------------------------------------------------------------------------------------------
@@ -49,30 +63,32 @@ void AtomDBProxy::pack_command_line_args() { tokenize(this->args); }
 
 void AtomDBProxy::tokenize(vector<string>& output) { BaseProxy::tokenize(output); }
 
-vector<string> AtomDBProxy::add_atoms(const vector<string>& tokens) {
-    vector<shared_ptr<Atom>> atoms = build_atoms_from_tokens(tokens);
+vector<string> AtomDBProxy::add_atoms(const vector<string>& tokens, bool use_streaming) {
+    vector<shared_ptr<Atom>> atoms =
+        build_atoms_from_tokens<shared_ptr<Atom>>(tokens, shared_ptr_atom_factory);
     vector<Atom*> atom_ptrs;
     for (const auto& atom : atoms) {
         atom_ptrs.push_back(atom.get());
     }
-    return add_atoms(atom_ptrs);
+    return add_atoms(atom_ptrs, use_streaming);
 }
 
-/**
- * Build a single argument vector containing, for each atom, a leading type
- * token ("NODE" or "LINK") followed by that atom's tokenized fields.
- */
-vector<string> AtomDBProxy::add_atoms(const vector<Atom*>& atoms) {
+vector<string> AtomDBProxy::add_atoms(const vector<Atom*>& atoms, bool use_streaming) {
     vector<string> args;
     vector<string> handles;
     string atom_type;
     vector<string> stream_info;
     stream_info.push_back(std::to_string(atoms.size()));
 
-    Utils::retry_function([&]() { this->to_remote_peer(AtomDBProxy::START_STREAM, stream_info); },
-                          5,
-                          2000,
-                          "AtomDBProxy::start_stream");
+    // Note to Reviewer: The streaming commands are currently disabled
+    // by default because the performance gain is negligible due to
+    // splitting the atoms into very small batches which is a temporary fix
+    // for a memory desallocation issue. Once that issue is fixed, streaming
+    // should be re-enabled by default.
+
+    if (use_streaming) {
+        set_stream(AtomDBProxy::START_STREAM, stream_info);
+    }
 
     for (Atom* atom : atoms) {
         atom->tokenize(args);
@@ -95,10 +111,9 @@ vector<string> AtomDBProxy::add_atoms(const vector<Atom*>& atoms) {
             args.clear();
         }
     }
-    Utils::retry_function([&]() { this->to_remote_peer(AtomDBProxy::END_STREAM, stream_info); },
-                          5,
-                          2000,
-                          "AtomDBProxy::end_stream");
+    if (use_streaming) {
+        set_stream(AtomDBProxy::END_STREAM, {});
+    }
 
     return handles;
 }
@@ -112,15 +127,19 @@ bool AtomDBProxy::from_remote_peer(const string& command, const vector<string>& 
     if (BaseProxy::from_remote_peer(command, args)) {
         return true;
     } else if (command == AtomDBProxy::ADD_ATOMS) {
-        handle_add_atoms(args);
+        if (this->is_processing_buffer) {
+            this->enqueue_request(args);
+        } else {
+            this->handle_add_atoms(args);
+        }
         return true;
     } else if (command == AtomDBProxy::START_STREAM) {
         LOG_INFO("Starting atom stream, total atoms to receive: " << args[0]);
-        this->processing_buffer = true;
+        this->is_processing_buffer = true;
         return true;
     } else if (command == AtomDBProxy::END_STREAM) {
         LOG_INFO("Ending atom stream.");
-        this->processing_buffer = false;
+        this->is_processing_buffer = false;
         return true;
     } else {
         Utils::error("Invalid AtomDBProxy command: <" + command + ">");
@@ -130,7 +149,8 @@ bool AtomDBProxy::from_remote_peer(const string& command, const vector<string>& 
 
 void AtomDBProxy::handle_add_atoms(const vector<string>& tokens) {
     try {
-        vector<shared_ptr<Atom>> atoms = build_atoms_from_tokens(tokens);
+        vector<shared_ptr<Atom>> atoms =
+            build_atoms_from_tokens<shared_ptr<Atom>>(tokens, shared_ptr_atom_factory);
         vector<Atom*> buffer;
         for (auto& atom : atoms) {
             buffer.push_back(atom.get());
@@ -141,31 +161,46 @@ void AtomDBProxy::handle_add_atoms(const vector<string>& tokens) {
     }
 }
 
-vector<shared_ptr<Atom>> AtomDBProxy::build_atoms_from_tokens(const vector<string>& tokens) {
-    vector<shared_ptr<Atom>> atoms;
-    string current;
-    vector<string> buffer;
-
-    auto flush = [&]() {
-        if (current.empty()) return;
-        if (current == NODE) {
-            atoms.push_back(make_shared<Node>(buffer));
-        } else {  // LINK
-            atoms.push_back(make_shared<Link>(buffer));
-        }
-        buffer.clear();
-    };
-
-    for (const auto& token : tokens) {
-        if (token == NODE || token == LINK) {
-            if (!current.empty()) flush();
-            current = token;
-        } else {
-            buffer.push_back(token);
-        }
+void AtomDBProxy::enqueue_request(const vector<string>& tokens) {
+    auto atoms = build_atoms_from_tokens<Atom*>(tokens, raw_ptr_atom_factory);
+    for (const auto& atom : atoms) {
+        this->processing_queue->enqueue((void*) atom);
     }
+    unique_lock<mutex> lock(this->api_mutex);
+    this->pending_atoms_count += atoms.size();
+}
 
-    if (!buffer.empty() && !current.empty()) flush();
+void AtomDBProxy::process_atom_batches() {
+    while (!this->stop_processing) {
+        if (this->pending_atoms_count >= AtomDBProxy::MAX_PENDING_ATOMS ||
+            (!this->is_processing_buffer && this->processing_queue->size() > 0)) {
+            unique_lock<mutex> lock(this->api_mutex);
+            vector<Atom*> atoms;
+            for (size_t i = 0; i < AtomDBProxy::BATCH_SIZE; ++i) {
+                if (this->processing_queue->empty()) {
+                    break;
+                }
+                auto atom = (Atom*) this->processing_queue->dequeue();
+                if (atom == nullptr) {
+                    Utils::error("Dequeued null atom pointer");
+                }
+                atoms.push_back(atom);
+            }
+            this->pending_atoms_count -= atoms.size();
+            lock.unlock();
+            auto job = [this, atoms]() {
+                this->atomdb->add_atoms(atoms, false, true);
+                for (auto& atom : atoms) {
+                    delete atom;
+                }
+            };
+            this->thread_pool->enqueue(job);
+        }
+        Utils::sleep();
+    }
+}
 
-    return atoms;
+void AtomDBProxy::set_stream(const string& command, const vector<string>& args) {
+    Utils::retry_function(
+        [&]() { this->to_remote_peer(command, args); }, 5, 2000, "AtomDBProxy::end_stream");
 }
--- src/agents/atomdb_broker/AtomDBProxy.h
+++ src/agents/atomdb_broker/AtomDBProxy.h
@@ -8,6 +8,8 @@
 #include "Atom.h"
 #include "AtomDB.h"
 #include "BaseProxy.h"
+#include "SharedQueue.h"
+#include "ThreadPool.h"
 
 using namespace std;
 using namespace agents;
@@ -27,9 +29,13 @@ class AtomDBProxy : public BaseProxy {
    public:
     // ---------------------------------------------------------------------------------------------
     // Proxy Commands
+    static int THREAD_POOL_SIZE;
+    static size_t MAX_PENDING_ATOMS;
     static string ADD_ATOMS;
     static string START_STREAM;
     static string END_STREAM;
+    static string NODE;
+    static string LINK;
 
     // ---------------------------------------------------------------------------------------------
     // Constructor and destructor
@@ -69,9 +75,9 @@ class AtomDBProxy : public BaseProxy {
      * @param atoms Vector of pointers to Atom to be sent.
      * @return Vector of handles corresponding to each atom.
      */
-    vector<string> add_atoms(const vector<Atom*>& atoms);
+    vector<string> add_atoms(const vector<Atom*>& atoms, bool use_streaming = false);
 
-    vector<string> add_atoms(const vector<string>& tokens);
+    vector<string> add_atoms(const vector<string>& tokens, bool use_streaming = false);
 
     // ---------------------------------------------------------------------------------------------
     // server-side API
@@ -98,15 +104,56 @@ class AtomDBProxy : public BaseProxy {
      * local AtomDB instance. Any exception during processing will be
      * reported back to the peer.
      */
+
     void handle_add_atoms(const vector<string>& args);
-    vector<shared_ptr<Atom>> build_atoms_from_tokens(const vector<string>& tokens);
+    template <typename AtomDataType, typename Factory>
+    std::vector<AtomDataType> build_atoms_from_tokens(const vector<string>& tokens, Factory&& factory) {
+        std::vector<AtomDataType> atoms;
+        std::string current;
+        std::vector<std::string> buffer;
+
+        auto flush = [&]() {
+            if (current.empty()) return;
+            atoms.emplace_back(factory(current, buffer));
+            buffer.clear();
+        };
+
+        for (const auto& t : tokens) {
+            if (t == NODE || t == LINK) {
+                if (!current.empty()) flush();
+                current = t;
+            } else {
+                buffer.push_back(t);
+            }
+        }
+
+        if (!current.empty()) flush();
+
+        return atoms;
+    }
+    static std::shared_ptr<Atom> shared_ptr_atom_factory(const string& type, vector<string>& data) {
+        if (type == NODE) return std::make_shared<Node>(data);
+        return std::make_shared<Link>(data);
+    }
+    static Atom* raw_ptr_atom_factory(const string& type, vector<string>& data) {
+        if (type == NODE) return new Node(data);
+        return new Link(data);
+    }
+    void enqueue_request(const vector<string>& tokens);
+    void process_atom_batches();
+    void set_stream(const string& command, const vector<string>& args);
 
     mutex api_mutex;
     shared_ptr<AtomDB> atomdb;
 
     static const size_t BATCH_SIZE;
     static const size_t COMMAND_SIZE_LIMIT;
-    bool processing_buffer = false;
+    bool is_processing_buffer = false;
+    SharedQueue* processing_queue = nullptr;
+    size_t pending_atoms_count = 0;
+    thread processing_thread;
+    ThreadPool* thread_pool;
+    bool stop_processing = false;
 };
 
 }  // namespace atomdb_broker
