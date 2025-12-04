6f5c92e19b659d76d406e987b1ca32bba947a527
Edgar Brissow <eddiebrissow@gmail.com>
2025-12-04 14:38:43 -0300

    Test refactoring

--- src/tests/cpp/atomdb_broker_test.cc
+++ src/tests/cpp/atomdb_broker_test.cc
@@ -29,6 +29,7 @@ class AtomDBTestEnvironment : public ::testing::Environment {
     void SetUp() override {
         TestConfig::load_environment();
         AtomDBSingleton::init();
+        ServiceBusSingleton::init("0.0.0.0:52001", "", 52003, 52999);
     }
 
     void TearDown() override {}
@@ -40,17 +41,48 @@ class AtomDBTest : public ::testing::Test {
     string atomdb_broker_server_id;
     string atomdb_broker_client_id;
     vector<string> added_atom_handles;
+    shared_ptr<ServiceBus> client_bus;
+    vector<Atom*> created_atoms;
 
     void SetUp() override {
-        query_agent_id = "0.0.0.0:52000";
-        atomdb_broker_server_id = "0.0.0.0:52001";
-        atomdb_broker_client_id = "0.0.0.0:52002";
         auto atomdb = AtomDBSingleton::get_instance();
         db = dynamic_pointer_cast<RedisMongoDB>(atomdb);
         ASSERT_NE(db, nullptr) << "Failed to cast AtomDB to RedisMongoDB";
+        // try {
+        query_agent_id = "0.0.0.0:52000";
+        atomdb_broker_server_id = "0.0.0.0:52001";
+        atomdb_broker_client_id = "0.0.0.0:52002";
+        shared_ptr<ServiceBus> service_bus = ServiceBusSingleton::get_instance();
+        service_bus->register_processor(make_shared<PatternMatchingQueryProcessor>());
+        Utils::sleep(500);
+        service_bus->register_processor(make_shared<AtomDBProcessor>());
+        Utils::sleep(500);
+        client_bus = make_shared<ServiceBus>(atomdb_broker_client_id, atomdb_broker_server_id);
+        Utils::sleep(500);
+    }
+    vector<Atom*> build_atoms(vector<string>& tokens) {
+        vector<Atom*> atoms;
+        for (auto& token : tokens) {
+            if (token.find("NODE") == 0) {
+                auto parts = Utils::split(token, ' ');
+                parts.erase(parts.begin());
+                atoms.push_back(new Node(parts));
+            } else if (token.find("LINK") == 0) {
+                auto parts = Utils::split(token, ' ');
+                parts.erase(parts.begin());
+                atoms.push_back(new Link(parts));
+            }
+        }
+        created_atoms.insert(created_atoms.end(), atoms.begin(), atoms.end());
+        return atoms;
     }
 
-    void TearDown() override {}
+    void TearDown() override {
+        for (auto atom : created_atoms) {
+            delete atom;
+        }
+        created_atoms.clear();
+    }
 
     string timestamp() {
         auto now = std::chrono::system_clock::now();
@@ -62,25 +94,9 @@ class AtomDBTest : public ::testing::Test {
 };
 
 TEST_F(AtomDBTest, AddAtoms) {
-    ServiceBusSingleton::init(query_agent_id, atomdb_broker_server_id, 52003, 52999);
-
-    shared_ptr<ServiceBus> service_bus = ServiceBusSingleton::get_instance();
-    service_bus->register_processor(make_shared<PatternMatchingQueryProcessor>());
-    Utils::sleep(500);
-
-    shared_ptr<ServiceBus> atomdb_broker_server_bus =
-        make_shared<ServiceBus>(atomdb_broker_server_id, query_agent_id);
-    Utils::sleep(500);
-    atomdb_broker_server_bus->register_processor(make_shared<AtomDBProcessor>());
-    Utils::sleep(500);
-
-    shared_ptr<ServiceBus> atomdb_broker_client_bus =
-        make_shared<ServiceBus>(atomdb_broker_client_id, atomdb_broker_server_id);
-    Utils::sleep(500);
-
     auto proxy = make_shared<AtomDBProxy>();
 
-    atomdb_broker_client_bus->issue_bus_command(proxy);
+    client_bus->issue_bus_command(proxy);
 
     string name1 = timestamp();
     string name2 = timestamp();
@@ -98,18 +114,7 @@ TEST_F(AtomDBTest, AddAtoms) {
     auto link = new Link("Expression", {node1, node2, node3}, true);
     string link_handle = link->handle();
 
-    vector<Atom*> atoms;
-    for (auto& token : tokens) {
-        if (token.find("NODE") == 0) {
-            auto parts = Utils::split(token, ' ');
-            parts.erase(parts.begin());
-            atoms.push_back(new Node(parts));
-        } else if (token.find("LINK") == 0) {
-            auto parts = Utils::split(token, ' ');
-            parts.erase(parts.begin());
-            atoms.push_back(new Link(parts));
-        }
-    }
+    auto atoms = build_atoms(tokens);
 
     EXPECT_TRUE(atoms[0]->handle() == node1);
     EXPECT_TRUE(atoms[1]->handle() == node2);
@@ -138,23 +143,9 @@ TEST_F(AtomDBTest, AddAtoms) {
 }
 
 TEST_F(AtomDBTest, AddAtomsStreaming) {
-    shared_ptr<ServiceBus> service_bus = ServiceBusSingleton::get_instance();
-    service_bus->register_processor(make_shared<PatternMatchingQueryProcessor>());
-    Utils::sleep(500);
-
-    shared_ptr<ServiceBus> atomdb_broker_server_bus =
-        make_shared<ServiceBus>(atomdb_broker_server_id, query_agent_id);
-    Utils::sleep(500);
-    atomdb_broker_server_bus->register_processor(make_shared<AtomDBProcessor>());
-    Utils::sleep(500);
-
-    shared_ptr<ServiceBus> atomdb_broker_client_bus =
-        make_shared<ServiceBus>(atomdb_broker_client_id, atomdb_broker_server_id);
-    Utils::sleep(500);
-
     auto proxy = make_shared<AtomDBProxy>();
 
-    atomdb_broker_client_bus->issue_bus_command(proxy);
+    client_bus->issue_bus_command(proxy);
 
     string name1 = timestamp();
     string name2 = timestamp();
@@ -172,18 +163,7 @@ TEST_F(AtomDBTest, AddAtomsStreaming) {
     auto link = new Link("Expression", {node1, node2, node3}, true);
     string link_handle = link->handle();
 
-    vector<Atom*> atoms;
-    for (auto& token : tokens) {
-        if (token.find("NODE") == 0) {
-            auto parts = Utils::split(token, ' ');
-            parts.erase(parts.begin());
-            atoms.push_back(new Node(parts));
-        } else if (token.find("LINK") == 0) {
-            auto parts = Utils::split(token, ' ');
-            parts.erase(parts.begin());
-            atoms.push_back(new Link(parts));
-        }
-    }
+    auto atoms = build_atoms(tokens);
 
     EXPECT_TRUE(atoms[0]->handle() == node1);
     EXPECT_TRUE(atoms[1]->handle() == node2);
@@ -212,23 +192,9 @@ TEST_F(AtomDBTest, AddAtomsStreaming) {
 }
 
 TEST_F(AtomDBTest, DeleteAtoms) {
-    shared_ptr<ServiceBus> service_bus = ServiceBusSingleton::get_instance();
-    service_bus->register_processor(make_shared<PatternMatchingQueryProcessor>());
-    Utils::sleep(500);
-
-    shared_ptr<ServiceBus> atomdb_broker_server_bus =
-        make_shared<ServiceBus>(atomdb_broker_server_id, query_agent_id);
-    Utils::sleep(500);
-    atomdb_broker_server_bus->register_processor(make_shared<AtomDBProcessor>());
-    Utils::sleep(500);
-
-    shared_ptr<ServiceBus> atomdb_broker_client_bus =
-        make_shared<ServiceBus>(atomdb_broker_client_id, atomdb_broker_server_id);
-    Utils::sleep(500);
-
     auto proxy = make_shared<AtomDBProxy>();
 
-    atomdb_broker_client_bus->issue_bus_command(proxy);
+    this->client_bus->issue_bus_command(proxy);
 
     proxy->delete_atoms(added_atom_handles, false);
 
@@ -237,6 +203,115 @@ TEST_F(AtomDBTest, DeleteAtoms) {
     for (const auto& handle : added_atom_handles) {
         EXPECT_FALSE(db->node_exists(handle));
     }
+}
+
+TEST_F(AtomDBTest, DeleteLinkTargetsTop) {
+    auto proxy = make_shared<AtomDBProxy>();
+
+    client_bus->issue_bus_command(proxy);
+
+    string name1 = timestamp();
+    string name2 = timestamp();
+    string name3 = timestamp();
+
+    string node1 = Hasher::node_handle("Symbol", name1);
+    string node2 = Hasher::node_handle("Symbol", name2);
+    string node3 = Hasher::node_handle("Symbol", name3);
+
+    vector<string> tokens = {"NODE Symbol false 3 is_literal bool false " + name1,
+                             "NODE Symbol false 3 is_literal bool true " + name2,
+                             "NODE Symbol false 3 is_literal bool true " + name3,
+                             "LINK Expression true 0 3 " + node1 + " " + node2 + " " + node3};
+
+    auto link = new Link("Expression", {node1, node2, node3}, true);
+    string link_handle = link->handle();
+
+    auto atoms = build_atoms(tokens);
+
+    EXPECT_TRUE(atoms[0]->handle() == node1);
+    EXPECT_TRUE(atoms[1]->handle() == node2);
+    EXPECT_TRUE(atoms[2]->handle() == node3);
+    EXPECT_TRUE(atoms[3]->handle() == link_handle);
+
+    EXPECT_FALSE(db->node_exists(node1));
+    EXPECT_FALSE(db->node_exists(node2));
+    EXPECT_FALSE(db->node_exists(node3));
+    EXPECT_FALSE(db->link_exists(link_handle));
+
+    vector<string> handles = proxy->add_atoms(atoms);
+
+    Utils::sleep(2000);
+
+    EXPECT_TRUE(db->node_exists(node1));
+    EXPECT_TRUE(db->node_exists(node2));
+    EXPECT_TRUE(db->node_exists(node3));
+    EXPECT_TRUE(db->link_exists(link_handle));
+
+    proxy->delete_atoms({link_handle}, true);
+
+    Utils::sleep(2000);
+    EXPECT_FALSE(db->link_exists(link_handle));
+    EXPECT_FALSE(db->node_exists(node1));
+    EXPECT_FALSE(db->node_exists(node2));
+    EXPECT_FALSE(db->node_exists(node3));
+}
+
+TEST_F(AtomDBTest, DeleteLinkTargetsBottom) {
+    auto proxy = make_shared<AtomDBProxy>();
+
+    client_bus->issue_bus_command(proxy);
+
+    string name1 = timestamp();
+    string name2 = timestamp();
+    string name3 = timestamp();
+    string name4 = timestamp();
+
+    string node1 = Hasher::node_handle("Symbol", name1);
+    string node2 = Hasher::node_handle("Symbol", name2);
+    string node3 = Hasher::node_handle("Symbol", name3);
+    string node4 = Hasher::node_handle("Symbol", name4);
+
+    vector<string> tokens = {"NODE Symbol false 3 is_literal bool false " + name1,
+                             "NODE Symbol false 3 is_literal bool true " + name2,
+                             "NODE Symbol false 3 is_literal bool true " + name3,
+                             "NODE Symbol false 3 is_literal bool true " + name4,
+                             "LINK Expression true 0 3 " + node1 + " " + node2 + " " + node3};
+
+    auto link = new Link("Expression", {node1, node2, node3}, true);
+    string link_handle = link->handle();
+
+    auto atoms = build_atoms(tokens);
+
+    EXPECT_TRUE(atoms[0]->handle() == node1);
+    EXPECT_TRUE(atoms[1]->handle() == node2);
+    EXPECT_TRUE(atoms[2]->handle() == node3);
+    EXPECT_TRUE(atoms[3]->handle() == node4);
+    EXPECT_TRUE(atoms[4]->handle() == link_handle);
+
+    EXPECT_FALSE(db->node_exists(node1));
+    EXPECT_FALSE(db->node_exists(node2));
+    EXPECT_FALSE(db->node_exists(node3));
+    EXPECT_FALSE(db->node_exists(node4));
+    EXPECT_FALSE(db->link_exists(link_handle));
+
+    vector<string> handles = proxy->add_atoms(atoms);
+
+    Utils::sleep(2000);
+
+    EXPECT_TRUE(db->node_exists(node1));
+    EXPECT_TRUE(db->node_exists(node2));
+    EXPECT_TRUE(db->node_exists(node3));
+    EXPECT_TRUE(db->node_exists(node4));
+    EXPECT_TRUE(db->link_exists(link_handle));
+
+    proxy->delete_atoms({node3}, true);
+
+    Utils::sleep(2000);
+    EXPECT_FALSE(db->link_exists(link_handle));
+    EXPECT_FALSE(db->node_exists(node1));
+    EXPECT_FALSE(db->node_exists(node2));
+    EXPECT_FALSE(db->node_exists(node3));
+    EXPECT_TRUE(db->node_exists(node4));
 }
 
 int main(int argc, char** argv) {
