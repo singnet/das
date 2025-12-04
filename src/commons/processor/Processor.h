f7c316c58a192e1d8ac5bbe97ae94c687d92e5f7
Andre Senna <andre.senna@gmail.com>
2025-12-04 08:53:47 -0300

    WIP

---
+++ src/commons/processor/Processor.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <mutex>
+
+using namespace std;
+
+namespace processor {
+
+/**
+ *
+ */
+class Processor {
+
+public:
+
+    enum State { UNDEFINED = 0, WAITING_SETUP, WAITING_START, WAITING_STOP, FINISHED };
+
+    Processor(const string& id);
+    ~Processor();
+    virtual void setup();
+    virtual void start();
+    virtual void stop();
+    bool is_setup();
+    bool is_finished();
+    string to_string();
+
+private:
+    void check_state(const string& action, State state);
+
+    State current_state;
+    string id;
+    mutex api_mutex;
+};
+
+} // namespace processor
