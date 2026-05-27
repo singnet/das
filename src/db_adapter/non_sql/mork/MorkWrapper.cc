f34d69f72206dcc3746d297878d62e88e22d7ac4
marcocapozzoli <marcocapozzoli90@gmail.com>
2026-05-27 14:33:56 -0300

    Refactor Metta2AtomsMapper and MorkWrapper to use queues for atom output and update constructor parameters

--- src/db_adapter/non_sql/mork/MorkWrapper.cc
+++ src/db_adapter/non_sql/mork/MorkWrapper.cc
@@ -15,17 +15,17 @@ using namespace db_adapter;
 //  Construction / destruction
 // ==============================
 
-MorkWrapper::MorkWrapper(MorkConnection& conn,
+MorkWrapper::MorkWrapper(shared_ptr<MorkConnection> conn,
                          shared_ptr<BoundedSharedQueue> output_queue,
                          MAPPER_TYPE mapper_type)
-    : DatabaseWrapper(conn, MapperFactory::create(mapper_type)),
+    : DatabaseWrapper(*conn, MapperFactory::create(mapper_type)),
       conn(conn),
       output_queue(output_queue) {}
 
 MorkWrapper::~MorkWrapper() {}
 
 void MorkWrapper::map(const string& metta_query) {
-    vector<string> metta_expressions = this->conn.query(metta_query);
+    vector<string> metta_expressions = this->conn->query(metta_query);
 
     if (metta_expressions.empty()) {
         RAISE_ERROR("No results returned from MORK query: " + metta_query);
@@ -34,25 +34,14 @@ void MorkWrapper::map(const string& metta_query) {
     for (const auto& expr : metta_expressions) {
         LOG_DEBUG("Mapping MORK expression: " << expr);
 
-        vector<shared_ptr<Atom>> atoms;
+        std::queue<shared_ptr<Atom>>* batch_queue = new std::queue<shared_ptr<Atom>>();
 
         try {
             MettaExpression metta_expr{expr};
-            atoms = this->mapper->map(DbInput{metta_expr});
-            LOG_DEBUG("Mapped " << atoms.size() << " atoms from MORK expression");
+            this->mapper->map(DbInput{metta_expr}, *batch_queue);
         } catch (const exception& e) {
             RAISE_ERROR("Error mapping MORK expression: " + expr + " : " + string(e.what()));
             return;
-        }
-
-        if (atoms.empty()) {
-            RAISE_ERROR("No atoms mapped from MORK expression: " + expr);
-        }
-
-        std::queue<shared_ptr<Atom>>* batch_queue = new std::queue<shared_ptr<Atom>>();
-
-        for (const auto& atom : atoms) {
-            batch_queue->push(atom);
         }
 
         unique_lock<mutex> lock(this->api_mutex);
