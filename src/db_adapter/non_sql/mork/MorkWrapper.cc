13fddfd5d23f01e508db99b914b98d7ad2dccc9c
marcocapozzoli <marcocapozzoli90@gmail.com>
2026-05-29 10:28:24 -0300

    Refactor DatabaseWrapper and its subclasses to use shared_ptr for DatabaseConnection

--- src/db_adapter/non_sql/mork/MorkWrapper.cc
+++ src/db_adapter/non_sql/mork/MorkWrapper.cc
@@ -18,7 +18,7 @@ using namespace db_adapter;
 MorkWrapper::MorkWrapper(shared_ptr<MorkConnection> conn,
                          shared_ptr<BoundedSharedQueue> output_queue,
                          MAPPER_TYPE mapper_type)
-    : DatabaseWrapper(*conn, MapperFactory::create(mapper_type)),
+    : DatabaseWrapper(conn, MapperFactory::create(mapper_type)),
       conn(conn),
       output_queue(output_queue) {}
 
