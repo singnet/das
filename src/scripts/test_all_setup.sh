4ced328e9401d2015336e131a34c42b74bea855e
CCG <artur.gontijo@singularitynet.io>
2026-07-29 15:06:48 -0300

    Fix test_all_setup.sh

--- src/scripts/test_all_setup.sh
+++ src/scripts/test_all_setup.sh
@@ -117,17 +117,20 @@ start_attention_broker(){
 }
 
 start_mork_server() {
-    echo "[SETUP-INFO] Starting MORK server..."
+    local name=$1
+    local port=$2
+
+    echo "[SETUP-INFO] Starting MORK ${name}..."
 
     docker run -d \
-        --name="mork-test-server" \
+        --name="${name}" \
         --network host \
         -e MORK_SERVER_ADDR=0.0.0.0 \
-        -e MORK_SERVER_PORT=40022 \
-        "${MORK_SERVER_IMAGE}" "$@"
+        -e MORK_SERVER_PORT=${port} \
+        "${MORK_SERVER_IMAGE}"
 
-    until curl --silent http://localhost:${DAS_MORK_PORT}/status/-; do
-    echo "[SETUP-INFO] Waiting for MORK..."
+    until curl --silent http://localhost:${port}/status/-; do
+    echo "[SETUP-INFO] Waiting for MORK ${name}..."
     sleep 1
     done
 }
@@ -185,12 +188,20 @@ else
     start_redis_mongo
 fi
 
-# 3. START MORK SERVER
+# 3.1 START MORK SERVER
 
 if container_running mork-test-server; then
     echo "[SETUP-INFO] Mork server container is already running. Skipping startup."
 else
-    start_mork_server
+    start_mork_server "mork-test-server" 40022
+fi
+
+# 3.2 START ADAPTERDB MORK SERVER
+
+if container_running adapterdb-mork-test-server; then
+    echo "[SETUP-INFO] AdapterDB MORK server container is already running. Skipping startup."
+else
+    start_mork_server "adapterdb-mork-test-server" 40032
 fi
 
 # 4.START POSTGRES SERVER
