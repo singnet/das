26a70570aea74eccf0b10a454cc7d5215651d8ea
Andre Senna <andre.senna@gmail.com>
2026-08-03 15:55:01 -0300

    Add default value to separator char

--- src/commons/Utils.h
+++ src/commons/Utils.h
@@ -120,7 +120,7 @@ class Utils {
     static void sleep(unsigned int milliseconds = 100);
     static string get_environment(string const& key);
     static map<string, string> parse_config(string const& config_path);
-    static vector<string> split(string const& str, char delimiter);
+    static vector<string> split(string const& str, char delimiter = ' ');
     static pair<size_t, size_t> parse_ports_range(string const& str, char delimiter = ':');
     static string join(vector<string> const& tokens, char delimiter = ' ');
     static string random_string(size_t length);
