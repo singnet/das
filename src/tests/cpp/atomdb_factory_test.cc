ba38fe82b85526844fee4a545b4fcffcdfe4d730
marcocapozzoli <marcocapozzoli90@gmail.com>
2026-09-08 12:16:28 -0300

    Change name from AtomDBPublicKeyAPI to AtomDBKeySensitive

--- src/tests/cpp/atomdb_factory_test.cc
+++ src/tests/cpp/atomdb_factory_test.cc
@@ -7,7 +7,7 @@
 
 #include "AdapterDB.h"
 #include "AtomDBFactory.h"
-#include "AtomDBKeySensitiveAPI.h"
+#include "AtomDBKeySensitive.h"
 #include "InMemoryDB.h"
 #include "JsonConfig.h"
 #include "MorkDB.h"
@@ -164,7 +164,7 @@ TEST(AtomDBFactoryTest, CreateInMemoryDBIsNotProtected) {
     ASSERT_NE(db, nullptr);
     EXPECT_EQ(db->get_protection_mode(), ProtectionMode::UNPROTECTED);
     EXPECT_EQ(dynamic_pointer_cast<ProtectedAtomDB>(db), nullptr);
-    EXPECT_EQ(dynamic_pointer_cast<AtomDBKeySensitiveAPI>(db), nullptr);
+    EXPECT_EQ(dynamic_pointer_cast<AtomDBKeySensitive>(db), nullptr);
 }
 
 TEST(AtomDBFactoryTest, CreateRemoteAtomDBIsNotWrapped) {
@@ -172,6 +172,6 @@ TEST(AtomDBFactoryTest, CreateRemoteAtomDBIsNotWrapped) {
     ASSERT_NE(db, nullptr);
     EXPECT_NE(dynamic_pointer_cast<RemoteAtomDB>(db), nullptr);
     EXPECT_EQ(dynamic_pointer_cast<ProtectedAtomDB>(db), nullptr);
-    EXPECT_NE(dynamic_pointer_cast<AtomDBKeySensitiveAPI>(db), nullptr);
+    EXPECT_NE(dynamic_pointer_cast<AtomDBKeySensitive>(db), nullptr);
     EXPECT_EQ(db->get_protection_mode(), ProtectionMode::UNPROTECTED);
 }
